d74e1133b5ad8746a529937369afc244e684cbc7
Ryan McCann <ryan_mccann@student.uml.edu>
2022-09-23 14:53:51 -0400

    [pylint] Update routing.py to pylint standards

--- code/routing.py
+++ code/routing.py
@@ -1,6 +1,6 @@
 import networkx as nx
 
 
-def routing(source, destination, physical_topology, slots_dictionary):
+def routing(source, destination, physical_topology, slots_dictionary):  # pylint: disable=unused-argument
     path = nx.shortest_path(G=physical_topology, source=source, target=destination)
     return path
