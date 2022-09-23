52d533ed6411b21645cb5f017225c3324b2218ad
Ryan McCann <ryan_mccann@student.uml.edu>
2022-09-23 14:46:37 -0400

    [pylint] Update load_input.py to pylint standards

--- code/load_input.py
+++ code/load_input.py
@@ -2,6 +2,6 @@ import json
 
 
 def load_input():
-    with open('../data/input.json') as json_file:
+    with open('../data/input.json', encoding='utf-8') as json_file:
         sim_input = json.load(json_file)
     return sim_input
