5e53d3c26f979ac80c6cee9bc9905f02495368c0
Ryan McCann <ryan_mccann@student.uml.edu>
2022-09-24 09:51:08 -0400

    [python] Add main function to Engine.py

--- code/Engine.py
+++ code/Engine.py
@@ -29,47 +29,52 @@ def create_PT(pt_input):
     return G, spectrum_DB
 
 
-for i in range(sim_input['NO_iteration']):
-    blocking_iter = 0
-    requests_status = {}
-    pt_res = create_PT(sim_input)
-    network_spectrum_DB = copy.deepcopy(pt_res[1])
-    Physical_topology = copy.deepcopy(pt_res[0])
+def main():
+    for i in range(sim_input['NO_iteration']):
+        blocking_iter = 0
+        requests_status = {}
+        pt_res = create_PT(sim_input)
+        network_spectrum_DB = copy.deepcopy(pt_res[1])
+        Physical_topology = copy.deepcopy(pt_res[0])
 
-    requests = Generate(seed_no=sim_input['seed'],
-                        nodes=list(sim_input['physical_topology']['nodes'].keys()),
-                        holding_time_mean=sim_input['holding_time_mean'],
-                        inter_arrival_time_mean=sim_input['inter_arrival_time'],
-                        req_no=sim_input['number_of_request'],
-                        slot_list=sim_input['BW_type'])
+        requests = Generate(seed_no=sim_input['seed'],
+                            nodes=list(sim_input['physical_topology']['nodes'].keys()),
+                            holding_time_mean=sim_input['holding_time_mean'],
+                            inter_arrival_time_mean=sim_input['inter_arrival_time'],
+                            req_no=sim_input['number_of_request'],
+                            slot_list=sim_input['BW_type'])
 
-    sorted_request = dict(sorted(requests.items()))
-    for time in sorted_request:
-        if sorted_request[time]['request_type'] == "Arrival":
-            rsa_res = controller_main(request_type="Release",
-                                      Physical_topology=Physical_topology,
-                                      network_spectrum_DB=network_spectrum_DB,
-                                      slot_NO=None,
-                                      path=None
-                                      )
-            if rsa_res is False:
-                blocking_iter += 1
-            else:
-                # for item in rsa_res[0]:
-                # TODO: A starting NO reserved slot will not exist for the "arrival" case
-                requests_status.update({sorted_request[time]['id']: {
-                    "slots": rsa_res[0]['starting_NO_reserved_slot'],
-                    "path": rsa_res[0]['path']
-                }})
-                network_spectrum_DB = rsa_res[1]
-                Physical_topology = rsa_res[2]
-        elif sorted_request[time]['request_type'] == "Release":
-            if sorted_request[time]['id'] in requests_status:
-                controller_main(request_type="Release",
-                                Physical_topology=Physical_topology,
-                                network_spectrum_DB=network_spectrum_DB,
-                                slot_NO=sorted_request[requests_status[time]['id']]['slots'],
-                                path=sorted_request[requests_status[time]['id']]['path']
-                                )
+        sorted_request = dict(sorted(requests.items()))
+        for time in sorted_request:
+            if sorted_request[time]['request_type'] == "Arrival":
+                rsa_res = controller_main(request_type="Release",
+                                          Physical_topology=Physical_topology,
+                                          network_spectrum_DB=network_spectrum_DB,
+                                          slot_NO=None,
+                                          path=None
+                                          )
+                if rsa_res is False:
+                    blocking_iter += 1
+                else:
+                    # for item in rsa_res[0]:
+                    # TODO: A starting NO reserved slot will not exist for the "arrival" case
+                    requests_status.update({sorted_request[time]['id']: {
+                        "slots": rsa_res[0]['starting_NO_reserved_slot'],
+                        "path": rsa_res[0]['path']
+                    }})
+                    network_spectrum_DB = rsa_res[1]
+                    Physical_topology = rsa_res[2]
+            elif sorted_request[time]['request_type'] == "Release":
+                if sorted_request[time]['id'] in requests_status:
+                    controller_main(request_type="Release",
+                                    Physical_topology=Physical_topology,
+                                    network_spectrum_DB=network_spectrum_DB,
+                                    slot_NO=sorted_request[requests_status[time]['id']]['slots'],
+                                    path=sorted_request[requests_status[time]['id']]['path']
+                                    )
 
-    blocking.update({i: blocking_iter / sim_input['number_of_request']})
+        blocking.update({i: blocking_iter / sim_input['number_of_request']})
+
+
+if __name__ == '__main__':
+    main()
