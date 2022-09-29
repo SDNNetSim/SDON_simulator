de8a065811523ec0baeb0e8cfd02a987d9abf2cb
Ryan McCann <ryan_mccann@student.uml.edu>
2022-09-29 15:47:04 -0400

    [spec_assignment] Change spectrum_assignment.py to have class functionality

--- code/spectrum_assignment.py
+++ code/spectrum_assignment.py
@@ -1,47 +1,49 @@
 import numpy as np
 
 
-def spectrum_assignment(src_dest: tuple, slots_needed, network_spectrum_DB):
-    """
-    Assigns the spectrum.
+class SpectrumAssignment:
+    def __init__(self, src_dest: tuple, slots_needed, network_spectrum_DB):
+        self.src_dest = src_dest
+        self.slots_needed = slots_needed
+        self.network_spectrum_DB = network_spectrum_DB
 
-    :return: The spectrum assignment (Always 1 as of now)
-    :rtype: int
-    """
-    try:
-        cores_matrix = network_spectrum_DB[src_dest]
-    except KeyError:
-        raise KeyError('Source to destination does not exist in the network spectrum database.')
+        self.num_slots = None
 
-    num_slots = np.shape(cores_matrix)[1]
+    def find_spectrum_slots(self, cores_matrix):
+        start_slot = 0
+        end_slot = self.slots_needed - 1
 
-    start_slot = 0
-    end_slot = slots_needed - 1
+        for core_num, core_arr in enumerate(cores_matrix):
+            open_slots_arr = np.where(core_arr == 0)[0]
+            # Look for a super spectrum in the current core
+            while end_slot < self.num_slots:
+                spectrum_set = set(core_arr[start_slot:end_slot])
+                # Spectrum is free
+                if spectrum_set == {0}:
+                    return {'core_num': core_num, 'start_slot': start_slot, 'end_slot': end_slot}
 
-    for core_num, core_arr in enumerate(cores_matrix):
-        # core_arr[33:75] = 1
-        # core_arr[0:] = 1
-        # core_arr[0:20] = 1
-        # core_arr[0:156] = 1
-        core_arr[0:157] = 1
+                # No more open slots
+                if len(open_slots_arr) == 0:
+                    break
 
-        open_slots_arr = np.where(core_arr == 0)[0]
-        # Look for a super spectrum in the current core
-        while end_slot < num_slots:
-            spectrum_set = set(core_arr[start_slot:end_slot])
-            # Spectrum is free
-            if spectrum_set == {0}:
-                return {'core_num': core_num, 'start_slot': start_slot, 'end_slot': end_slot}
+                # TODO: This will check zero twice potentially
+                # Jump to next available slot, assume window will shift therefore we can never pick index 0
+                start_slot = open_slots_arr[0]
+                # Remove prior slots
+                open_slots_arr = open_slots_arr[1:]
+                end_slot = start_slot + (self.slots_needed - 1)
 
-            # No more open slots
-            if len(open_slots_arr) == 0:
-                break
+        return False
 
-            # TODO: This will check zero more than once
-            # Jump to next available slot, assume window will shift therefore we can never pick index 0
-            start_slot = open_slots_arr[0]
-            # Remove prior slots
-            open_slots_arr = open_slots_arr[1:]
-            end_slot = start_slot + (slots_needed - 1)
+    def find_src_dest(self):
+        try:
+            cores_matrix = self.network_spectrum_DB[self.src_dest]
+        except KeyError:
+            raise KeyError('Source to destination does not exist in the network spectrum database.')
 
-    return False
+        self.num_slots = np.shape(cores_matrix)[1]
+        return cores_matrix
+
+    def find_free_spectrum(self):
+        cores_matrix = self.find_src_dest()
+        self.find_spectrum_slots(cores_matrix=cores_matrix)
--- tests/test_spectrum_assignment.py
+++ tests/test_spectrum_assignment.py
@@ -4,6 +4,10 @@ import numpy as np
 
 
 class TestSpectrumAssignment(unittest.TestCase):
+    # core_arr[33:75] = 1
+    # core_arr[0:] = 1
+    # core_arr[0:20] = 1
+    # core_arr[0:156] = 1
     def test_zeros_arr(self):
         pass
 
