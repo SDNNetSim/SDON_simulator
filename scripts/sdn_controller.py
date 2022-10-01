632d1b661b368541fd65e5f05c05638bc702d44d
Ryan McCann <ryan_mccann@student.uml.edu>
2022-10-01 11:02:24 -0400

    [sphinx] Document sdn_controller.py

--- scripts/sdn_controller.py
+++ scripts/sdn_controller.py
@@ -6,14 +6,14 @@ def release(network_spec_db, path, slot_num, num_occ_slots):
     """
     Releases the slots in the network spectrum database e.g. sets them back to zero.
 
-    :param network_spectrum_DB: The current network spectrum database
-    :type network_spectrum_DB: dict
+    :param network_spec_db: The current network spectrum database
+    :type network_spec_db: dict
     :param path: The shortest path computed
     :type path: list
-    :param slot_NO: The first slot number taken
-    :type slot_NO: int
-    :param No_occupied_slots: The total number of slots occupied
-    :type No_occupied_slots: int
+    :param slot_num: The first slot number taken
+    :type slot_num: int
+    :param num_occ_slots: The total number of slots occupied
+    :type num_occ_slots: int
     :return: The updated network spectrum database
     :rtype: dict
     """
@@ -30,14 +30,20 @@ def controller_main(src, dest, request_type, physical_topology, network_spec_db,
     """
     Either releases spectrum from the database, assigns a spectrum, or returns False otherwise.
 
+    :param src: The source node
+    :type src: str
+    :param dest: The destination node
+    :type dest: str
     :param request_type: The request type e.g. Release
     :type request_type: str
-    :param Physical_topology: Holds all the physical topology variables
-    :type Physical_topology: dict
-    :param network_spectrum_DB: Holds the network spectrum information
-    :type network_spectrum_DB: dict
-    :param slot_NO: The starting slot number desired
-    :type slot_NO: int
+    :param physical_topology: Holds all the physical topology variables
+    :type physical_topology: dict
+    :param network_spec_db: Holds the network spectrum information
+    :type network_spec_db: dict
+    :param slots_needed: The number of spectrum slots needed
+    :type slots_needed: int
+    :param slot_num: The starting slot number desired
+    :type slot_num: int
     :param path: The shortest path found
     :type path: list
     """
