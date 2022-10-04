13e236bd45db648c8224f55a51999cdc4538a960
Ryan McCann <ryan_mccann@student.uml.edu>
2022-10-04 11:44:31 -0400

    [routing] Finished routing functionality

--- scripts/routing.py
+++ scripts/routing.py
@@ -12,21 +12,21 @@ class Routing:
         self.network_spec_db = network_spec_db
         self.slots_needed = slots_needed
 
-        self.paths_dict = dict()
+        self.paths_list = list()
 
     def find_least_cong_route(self):
         # Sort dictionary by number of slots occupied key and return the first one
-        res = sorted(self.paths_dict.items(), key=lambda x: x[1]['slots_taken'])
-        print(res)
-        return list()
+        sorted_paths_list = sorted(self.paths_list, key=lambda d: d['link_info']['slots_taken'])
+
+        return sorted_paths_list[0]['path']
 
     def find_most_cong_link(self, path):
         # Would just be comparing the lengths of finding the non-zero slots of numpy arrays
-        res_dict = {'path': path, 'link': None, 'core': None, 'slots_taken': -1}
+        res_dict = {'link': None, 'core': None, 'slots_taken': -1}
 
         for i in range(len(path) - 1):
-            cores_matrix = self.network_spec_db[(path[i]), path[i+1]]['cores_matrix']
-            link_num = self.network_spec_db[(path[i]), path[i+1]]['link_num']
+            cores_matrix = self.network_spec_db[(path[i]), path[i + 1]]['cores_matrix']
+            link_num = self.network_spec_db[(path[i]), path[i + 1]]['link_num']
 
             for core_num, core_arr in enumerate(cores_matrix):
                 slots_taken = len(np.where(core_arr == 1)[0])
@@ -35,7 +35,8 @@ class Routing:
                     res_dict['core'] = core_num
                     res_dict['link'] = link_num
 
-        self.paths_dict[path] = res_dict
+        # Link info is information about the most congested link found
+        self.paths_list.append({'path': path, 'link_info': res_dict})
 
     def least_congested_path(self):
         paths_obj = nx.all_simple_paths(G=self.physical_topology, source=self.source, target=self.destination)
@@ -44,11 +45,11 @@ class Routing:
         min_hops = None
 
         for i, path in enumerate(paths_matrix):
+            num_hops = len(path)
             if i == 0:
-                min_hops = len(path)
+                min_hops = num_hops
                 self.find_most_cong_link(path)
             else:
-                num_hops = len(path)
                 if num_hops <= min_hops + 1:
                     self.find_most_cong_link(path)
                 else:
