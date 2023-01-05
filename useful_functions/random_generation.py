2290d1c6c2ddcea39d3fa5970dc8f2dff0f00c07
Ryan McCann <ryan_mccann@student.uml.edu>
2023-01-05 17:48:09 -0500

    [random] Remove universal rv function

--- useful_functions/random_generation.py
+++ useful_functions/random_generation.py
@@ -12,18 +12,6 @@ def set_seed(seed_no):
     np.random.seed(seed_no)
 
 
-def universal_rv():
-    """
-    Generates a random value between zero and one.
-
-    :return: A random value between zero and one
-    :rtype: float
-    """
-    # TODO: Ask about this (compare graphs 0104_18:23:27 and 0104_18:44:47)
-    # return float(np.random.randint(0, 2147483647)) / float(2147483647)
-    return np.random.uniform(0, 1)
-
-
 def uniform_rv(scale_param=None):
     """
     Generates a value from a uniform distribution. Optional scale parameter.
@@ -49,6 +37,4 @@ def exponential_rv(scale_param):
     :rtype: float
     """
     # np.log is the natural logarithm
-    # TODO: Ask about this (above)
-    # return ((-1.0) / float(scale_param)) * np.log(universal_rv())
     return ((-1.0) / float(scale_param)) * np.log(uniform_rv())
