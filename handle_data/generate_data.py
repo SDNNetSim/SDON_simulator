3360659f1581f23b28eabcae6a063a4585513203
Ryan McCann <ryan_mccann@student.uml.edu>
2023-01-08 15:28:47 -0500

    [data] Add new modulation formats
    - Also get rid of the useless calculation in create_bw_info

--- handle_data/generate_data.py
+++ handle_data/generate_data.py
@@ -50,13 +50,17 @@ def create_bw_info(bw_slot=12.5, assume=None):
     # Max length is in km
     if assume == 'yue':
         bw_info = {
-            '50': {'QPSK': {'max_length': 11080}, '16-QAM': {'max_length': 4750}, '64-QAM': {'max_length': 1832}},
-            '100': {'QPSK': {'max_length': 5540}, '16-QAM': {'max_length': 2375}, '64-QAM': {'max_length': 916}},
-            '400': {'QPSK': {'max_length': 1385}, '16-QAM': {'max_length': 594}, '64-QAM': {'max_length': 229}},
+            '25': {'QPSK': {'max_length': 22160, 'slots_needed': 1}, '16-QAM': {'max_length': 9500, 'slots_needed': 1},
+                   '64-QAM': {'max_length': 3664, 'slots_needed': 1}},
+            '50': {'QPSK': {'max_length': 11080, 'slots_needed': 2}, '16-QAM': {'max_length': 4750, 'slots_needed': 1},
+                   '64-QAM': {'max_length': 1832, 'slots_needed': 1}},
+            '100': {'QPSK': {'max_length': 5540, 'slots_needed': 4}, '16-QAM': {'max_length': 2375, 'slots_needed': 2},
+                    '64-QAM': {'max_length': 916, 'slots_needed': 2}},
+            '200': {'QPSK': {'max_length': 2770, 'slots_needed': 8}, '16-QAM': {'max_length': 1187, 'slots_needed': 4},
+                    '64-QAM': {'max_length': 458, 'slots_needed': 3}},
+            '400': {'QPSK': {'max_length': 1385, 'slots_needed': 16}, '16-QAM': {'max_length': 594, 'slots_needed': 8},
+                    '64-QAM': {'max_length': 229, 'slots_needed': 6}},
         }
-        for bw, bw_obj in bw_info.items():  # pylint: disable=invalid-name
-            for mod_format, mod_obj in bw_obj.items():  # pylint: disable=unused-variable
-                bw_obj[mod_format]['slots_needed'] = math.ceil(float(bw) / bw_slot)   # pylint: disable=unnecessary-dict-index-lookup
     elif assume == 'arash':
         bw_info = {
             '100': {'QPSK': {'slots_needed': 3}},
