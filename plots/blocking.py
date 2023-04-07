dd5504067efbdeb100f95bf23d96bbb1ccbea83e
Ryan McCann <ryan_mccann@student.uml.edu>
2023-04-07 16:46:39 -0400

    Update request_generator.py with ChatGPT

--- plots/blocking.py
+++ plots/blocking.py
@@ -484,7 +484,7 @@ def main():
     blocking_obj = Blocking()
 
     # blocking_obj.des_times = ['0329/11:50:35', '0323/09:22:02', '0323/09:22:04']
-    blocking_obj.des_times = ['0407/15:53:41']
+    blocking_obj.des_times = ['0407/16:39:42']
     blocking_obj.policy = 'First Fit'
     blocking_obj.weighted = False
     blocking_obj.cong_only = False
--- sim_scripts/request_generator.py
+++ sim_scripts/request_generator.py
@@ -1,97 +1,86 @@
+# Local application imports
 from useful_functions.random_generation import set_seed, uniform_rv, exponential_rv
 
 
-def generate(seed, nodes, hold_time_mean, arr_rate_mean, num_reqs, mod_per_bw, req_dist):
+def generate(seed: int, nodes: list, hold_time_mean: float, arr_rate_mean: float,
+             num_reqs: int, mod_per_bw: dict, req_dist: dict):
     """
-    Generate all the requests for the simulation.
+    Generates requests for a simulation.
 
-    :param seed: The seed
+    :param seed: Seed for random number generation.
     :type seed: int
-    :param nodes: All nodes in the network
+
+    :param nodes: List of nodes in the network.
     :type nodes: list
-    :param mu: The holding time mean
-    :type mu: float
-    :param lam: The inter-arrival time mean
-    :type lam: float
-    :param num_requests: Amount of requests
-    :type num_requests: int
-    :param bw_dict: Contains information for each bandwidth and modulation
-    :type bw_dict: dict
-    :param req_dist: The distribution of requests we'd like to generate
+
+    :param hold_time_mean: Mean hold time of a request.
+    :type hold_time_mean: float
+
+    :param arr_rate_mean: Mean arrival rate of requests.
+    :type arr_rate_mean: float
+
+    :param num_reqs: Number of requests to generate.
+    :type num_reqs: int
+
+    :param mod_per_bw: Dictionary with modulation formats for each bandwidth.
+    :type mod_per_bw: dict
+
+    :param req_dist: Dictionary with the distribution of requests among different bandwidths.
     :type req_dist: dict
-    :return: Every request generated
+
+    :return: A dictionary with the generated requests.
     :rtype: dict
     """
+
+    # Initialize variables
     requests = {}
     current_time = 0
-    # We must start at 1, as we allocate spectral slots with the request number and '0' is considered a free spectral
-    # slot
     counter_id = 1
-
     set_seed(seed=seed)
+    bandwidth_counts = {bandwidth: int(req_dist[bandwidth] * num_reqs) for bandwidth in
+                        mod_per_bw}
+    bandwidth_list = list(mod_per_bw.keys())
 
-    # Number of requests allocated for each bandwidth
-    # TODO: This could potentially not equal the number of requests we think
-    bw_one_req = req_dist['25'] * num_reqs
-    bw_two_req = req_dist['50'] * num_reqs
-    bw_three_req = req_dist['100'] * num_reqs
-    bw_four_req = req_dist['200'] * num_reqs
-    bw_five_req = req_dist['400'] * num_reqs
-
-    # Monitor the number of requests allocated for each bandwidth
-    bands_dict = {'25': bw_one_req, '50': bw_two_req, '100': bw_three_req, '200': bw_four_req, '400': bw_five_req}
-    # List of all possible bandwidths
-    bands_list = list(mod_per_bw.keys())
-
-    # Multiplied by two, to account for arrival and departure requests
+    # Generate requests
     while len(requests) < (num_reqs * 2):
-        current_time = current_time + exponential_rv(arr_rate_mean)
+        current_time += exponential_rv(arr_rate_mean)
         depart_time = current_time + exponential_rv(hold_time_mean)
 
-        # We never want our node to equal the length, we start from index 0 in a list! (Node numbers are all minus 1)
-        src = nodes[uniform_rv(len(nodes))]
-        des = nodes[uniform_rv(len(nodes))]
+        source = nodes[uniform_rv(len(nodes))]
+        dest = nodes[uniform_rv(len(nodes))]
 
-        while src == des:
-            des = nodes[uniform_rv(len(nodes))]
+        while dest == source:
+            dest = nodes[uniform_rv(len(nodes))]
 
         while True:
-            chosen_bw = bands_list[uniform_rv(len(bands_list))]
-            if bands_dict[chosen_bw] > 0:
-                bands_dict[chosen_bw] -= 1
+            chosen_bandwidth = bandwidth_list[uniform_rv(len(bandwidth_list))]
+            if bandwidth_counts[chosen_bandwidth] > 0:
+                bandwidth_counts[chosen_bandwidth] -= 1
                 break
 
         if current_time not in requests and depart_time not in requests:
             requests.update({current_time: {
                 "id": counter_id,
-                "source": src,
-                "destination": des,
+                "source": source,
+                "destination": dest,
                 "arrive": current_time,
                 "depart": depart_time,
                 "request_type": "arrival",
-                "bandwidth": chosen_bw,
-                "mod_formats": mod_per_bw[chosen_bw],
-                "start_slot_no": None,
-                "working_path": None,
-                "protection_path": None
+                "bandwidth": chosen_bandwidth,
+                "mod_formats": mod_per_bw[chosen_bandwidth],
             }})
             requests.update({depart_time: {
                 "id": counter_id,
-                "source": src,
-                "destination": des,
+                "source": source,
+                "destination": dest,
                 "arrive": current_time,
                 "depart": depart_time,
                 "request_type": "release",
-                "bandwidth": chosen_bw,
-                "mod_formats": mod_per_bw[chosen_bw],
-                "start_slot_no": None,
-                "working_path": None,
-                "protection_path": None
+                "bandwidth": chosen_bandwidth,
+                "mod_formats": mod_per_bw[chosen_bandwidth],
             }})
             counter_id += 1
-        # Bandwidth wasn't chosen due to time conflict, request not allocated (previously subtracted)
         else:
-            bands_dict[chosen_bw] += 1
-            continue
+            bandwidth_counts[chosen_bandwidth] += 1
 
     return requests
