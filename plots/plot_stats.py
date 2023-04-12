21a1307f2ec946a39d4b2c872b51a22d9cfa755c
Ryan McCann <ryan_mccann@student.uml.edu>
2023-04-12 19:24:10 -0400

    Update plot_stats.py with ChatGPT

--- plots/plot_stats.py
+++ plots/plot_stats.py
@@ -14,13 +14,15 @@ class PlotStats:
     A class for computing and plotting statistical analysis for simulations.
     """
 
-    def __init__(self, net_name: str, data_dir: str = None):
+    def __init__(self, net_name: str, data_dir: str = None, latest_date: str = None, latest_time: str = None):
         """
         Initializes the PlotStats class.
         """
         # Information to retrieve desired data
         self.net_name = net_name
         self.data_dir = data_dir
+        self.latest_date = latest_date
+        self.latest_time = latest_time
         self.file_info = self.get_file_info()
 
         # The final dictionary containing information for all plots
@@ -29,6 +31,7 @@ class PlotStats:
         self.colors = ['#024de3', '#00b300', 'orange', '#6804cc', '#e30220']
         self.line_styles = ['solid', 'dashed', 'dotted', 'dashdot']
         self.markers = {1: 'o', 2: '^', 4: 's', 8: 'x'}
+        self.x_ticks = [10, 100, 200, 300, 400]
 
         self.get_data()
 
@@ -40,10 +43,10 @@ class PlotStats:
         if self.data_dir is None:
             self.data_dir = f'../data/output/{self.net_name}/'
             dir_list = os.listdir(self.data_dir)
-            latest_date = sorted(dir_list)[-1]
-            dir_list = os.listdir(os.path.join(self.data_dir, latest_date))
-            latest_time = sorted(dir_list)[-1]
-            self.data_dir = os.path.join(self.data_dir, f'{latest_date}/{latest_time}')
+            self.latest_date = sorted(dir_list)[-1]
+            dir_list = os.listdir(os.path.join(self.data_dir, self.latest_date))
+            self.latest_time = sorted(dir_list)[-1]
+            self.data_dir = os.path.join(self.data_dir, f'{self.latest_date}/{self.latest_time}')
 
         files_dict = {}
         for thread in os.listdir(self.data_dir):
@@ -62,7 +65,7 @@ class PlotStats:
             self.plot_dict[thread] = {
                 'erlang_vals': [],
                 'blocking_vals': [],
-                'average_transistors': [],
+                'average_transponders': [],
                 'distance_block': [],
                 'cong_block': [],
                 'hold_time_mean': None,
@@ -86,7 +89,7 @@ class PlotStats:
                 else:
                     self.plot_dict[thread]['blocking_vals'].append(erlang_dict['misc_stats']['blocking_mean'])
 
-                self.plot_dict[thread]['average_transistors'].append(erlang_dict['misc_stats']['trans_mean'])
+                self.plot_dict[thread]['average_transponders'].append(erlang_dict['misc_stats']['trans_mean'])
                 self.plot_dict[thread]['distance_block'].append(erlang_dict['misc_stats']['dist_percent'])
                 self.plot_dict[thread]['cong_block'].append(erlang_dict['misc_stats']['cong_percent'])
 
@@ -96,7 +99,7 @@ class PlotStats:
 
                 self.plot_dict[thread]['max_slices'] = erlang_dict['misc_stats']['max_slices']
 
-                if erlang == 10 or erlang == 100 or erlang == 700:
+                if erlang in (10, 100, 700):
                     self.plot_dict[thread]['taken_slots'][erlang] = dict()
                     self.plot_dict[thread]['num_slices'][erlang] = list()
 
@@ -106,160 +109,101 @@ class PlotStats:
 
                         self.plot_dict[thread]['num_slices'][erlang].append(request_info['num_slices'])
 
-    def _save_plot(self):
-        pass
+    def _save_plot(self, file_name):
+        """
+        Save a Matplotlib plot to a file with the specified name.
 
-    def _show_plot(self):
-        pass
+        :param file_name: A string representing the name of the file to save the plot as
+        :type file_name: str
+        """
+        file_path = f'./output/{self.net_name}/{self.latest_date}/{self.latest_time}/'
+        create_dir(file_path)
+        plt.savefig(f'{file_path}/{file_name}.png')
 
-    def _setup_plots(self):
-        pass
+    def _setup_plot(self, title, y_label, x_label, grid=True):
+        """
+        Set up a Matplotlib plot with a given title, y-axis label, and x-axis label.
+
+        :param title: A string representing the title of the plot.
+        :type title: str
+
+        :param y_label: A string representing the label for the y-axis of the plot.
+        :type y_label: str
+
+        :param x_label: A string representing the label for the x-axis of the plot.
+        :type x_label: str
+
+        :param grid: A boolean indicating whether to show a grid on the plot.
+        :type grid: bool
+        """
+        plt.figure(figsize=(7, 5), dpi=300)
+        plt.title(title)
+        plt.ylabel(y_label)
+        plt.xlabel(x_label)
+
+        plt.xticks(self.x_ticks)
+        plt.xlim(self.x_ticks[0], self.x_ticks[-1])
+
+        if grid:
+            plt.grid()
 
     def plot_blocking(self):
-        plt.figure(figsize=(7, 5), dpi=300)
-        plt.title(f'{self.net_name} LS = 8 BP vs. Erlang ({self.policy})')
-        plt.ylabel('Blocking Probability')
+        """
+        Plots the blocking probability for each Erlang value.
+        """
+        self._setup_plot(f'{self.net_name} BP vs. Erlang', 'Blocking Probability', 'Erlang')
+
         plt.ylim(10 ** -5, 1)
         plt.yscale('log')
 
-        plt.grid()
-        plt.xlabel('Erlang')
+        style_count = 0
+        legend_list = list()
+        for thread, thread_obj in self.plot_dict.items():  # pylint: disable=unused-variable
+            color = self.colors[style_count]
+            line_style = self.line_styles[style_count]
+            marker = self.markers[thread_obj['max_slices']]
 
-        count = 0
-        for curr_time, thread_obj in self.plot_dict.items():
-            # color = self.colors[count]
-            line_style = self.line_styles[count]
-            for thread, info_obj in thread_obj.items():  # pylint: disable=unused-variable
-                # LS = 16 considered irrelevant for the time being
-                color = self.colors[count]
-                # if info_obj['max_lps'] != 8 and info_obj['max_lps'] != 8:
-                #     continue
-                # if info_obj['max_lps'] != 8:
-                #     continue
-                marker = self.markers[info_obj['max_lps']]
-                plt.plot(info_obj['erlang'], info_obj['blocking'], color=color, linestyle=line_style,
-                         marker=marker, markersize=2.3)
-                # plt.plot(info_obj['erlang'], info_obj['blocking'], color=color,
-                #          markersize=2.3)
-                self.legend_list.append(f"C ={info_obj['num_cores']} LS ={info_obj['max_lps']}")
-                # self.legend_list.append(f"C ={info_obj['num_cores']}")
+            plt.plot(thread_obj['erlang_vals'], thread_obj['blocking_vals'], color=color, linestyle=line_style,
+                     marker=marker, markersize=2.3)
+            legend_list.append(f"C ={thread_obj['cores_per_link']} LS ={thread_obj['max_slices']}")
 
-                count += 1
-            # count += 1
+            style_count += 1
 
         plt.yticks([10 ** -4, 10 ** -3, 10 ** -2, 10 ** -1, 1])
-        plt.xticks(self.x_axis)
-        plt.xlim(self.x_axis[0], self.x_axis[-1])
-        plt.legend(self.legend_list)
+        plt.legend(legend_list)
 
-        self.save_show_plot(file_name='blocking')
+        self._save_plot(file_name='blocking')
+        plt.show()
 
     def plot_transponders(self):
         """
-        Plots the average number of transponders
+        Plots the average number of transponders used for each Erlang value.
         """
-        plt.figure(figsize=(7, 5), dpi=300)
-        plt.title(f'{self.network_name} LS = 8 Transponders vs. Erlang ({self.policy})')
-        plt.ylabel('Transponders per Request')
-
-        plt.grid()
-        plt.xlabel('Erlang')
+        self._setup_plot(f'{self.net_name} Transponders vs. Erlang', 'Transponders', 'Erlang')
+        plt.ylim(0.9, 2)
 
         legend_list = list()
-        count = 0
-        for curr_time, obj in self.plot_dict.items():
-            for thread, obj_2 in obj.items():  # pylint: disable=unused-variable
-                color = self.colors[count]
-                # if obj_2['max_lps'] != 8:
-                #     continue
-                marker = self.markers[obj_2['max_lps']]
-                plt.plot(obj_2['erlang'], obj_2['av_trans'], color=color, markersize=2.3)
-                # legend_list.append(f"C={obj_2['num_cores']} LS={obj_2['max_lps']}")
-                legend_list.append(f"C={obj_2['num_cores']}")
+        style_count = 0
+        for thread, thread_obj in self.plot_dict.items():  # pylint: disable=unused-variable
+            color = self.colors[style_count]
 
-                count += 1
-            # count += 1
+            plt.plot(thread_obj['erlang_vals'], thread_obj['average_transponders'], color=color)
+            legend_list.append(f"C={thread_obj['cores_per_link']} LS={thread_obj['max_slices']}")
+
+            style_count += 1
 
         plt.legend(legend_list)
-        plt.xticks(self.x_axis)
-        plt.xlim(self.x_axis[0], self.x_axis[-1])
-        plt.ylim(0.9, 2)
-        self.save_show_plot(file_name='transponders')
-
-    def plot_slots_taken(self):
-        plt.figure(figsize=(7, 5), dpi=300)
-        # plt.title(f'{self.network_name} Request Snapshot vs. Slots Occupied (C = {self.num_cores})')
-        plt.title(f'{self.network_name} Unlimited Slicing Request Snapshot vs. Slots Occupied Norm')
-        plt.ylabel('Slots Occupied')
-
-        plt.grid()
-        plt.xlabel('Request Number')
-
-        legend_list = list()
-        marker_lst = ['o', '^', 's', 'x']
-        count = 0
-        marker_count = 0
-        for curr_time, obj in self.plot_dict.items():
-            for thread, obj_2 in obj.items():  # pylint: disable=unused-variable
-                for erlang, obj_3 in obj_2['occ_slots'].items():
-                    # if erlang == 700 and obj_2['max_lps'] == 8:
-                    #     continue
-                    color = self.colors[count]
-                    marker = marker_lst[marker_count]
-
-                    # print(self.num_cores)
-                    # obj_3['occ_slots'] = [val / self.num_cores for val in obj_3['occ_slots']]
-                    obj_3['occ_slots'] = [val / obj_2['num_cores'] for val in obj_3['occ_slots']]
-                    plt.plot(obj_3['req_ids'], obj_3['occ_slots'], color=color, marker=marker, markersize=2.3)
-                    # legend_list.append(f"E={int(erlang)} LS={obj_2['max_lps']}")
-                    legend_list.append(f"E={int(erlang)} C={obj_2['num_cores']}")
-                    # count += 1
-                    marker_count += 1
-                    if marker_count == 3:
-                        marker_count = 0
-                count += 1
-
-        plt.legend(legend_list, loc='upper left')
-        plt.xlim(0, 10000)
-        plt.ylim(0, 4100)
-        self.save_show_plot(file_name='slots_occupied')
-
-    def plot_num_slices(self):
-        plt.figure(figsize=(7, 5), dpi=300)
-        # plt.title(f'{self.network_name} Number of Slices vs. Occurrences (C = {self.num_cores})')
-        plt.title(f'{self.network_name} Number of Slices vs. Occurrences (Unlimited)')
-        plt.ylabel('Occurrences')
-
-        plt.xlabel('Number of Slices')
-
-        slice_colors = ['#0000b3', '#3333ff', '#9999ff', '#b30000', '#ff3333', '#ff9999', '#00b33c', '#00ff55',
-                        '#99ffbb', '#b36b00', '#ff9900', '#ffb84d']
-
-        count = 0
-        res_list = list()
-        legend_list = list()
-        for curr_time, obj in self.plot_dict.items():
-            for thread, obj_2 in obj.items():
-                if obj_2['max_lps'] == 1:
-                    continue
-                for erlang, slice_lst in obj_2['num_slices'].items():
-                    res_list.append(slice_lst)
-                    legend_list.append(f"E={int(erlang)} C={obj_2['num_cores']}")
-                    count += 1
-                # count += 1
-
-        plt.hist(res_list,
-                 stacked=False, histtype='bar', edgecolor='black', rwidth=1, color=slice_colors)
-
-        plt.ylim(0, 10000)
-        plt.xlim(0, 8)
-        plt.legend(legend_list, loc='upper right')
-        self.save_show_plot(file_name='num_slices')
+        self._save_plot(file_name='transponders')
+        plt.show()
 
 
 def main():
-    test_obj = PlotStats(net_name='USNet')
-    test_obj.plot_blocking()
+    """
+    Controls this script.
+    """
+    plot_obj = PlotStats(net_name='USNet')
+    plot_obj.plot_blocking()
+    plot_obj.plot_transponders()
 
 
 if __name__ == '__main__':
