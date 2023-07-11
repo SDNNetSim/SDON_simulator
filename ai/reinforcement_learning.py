f743a1d4f746f221cde9d017b246c60f938ddd3c
Ryan McCann <ryan_mccann@student.uml.edu>
2023-07-11 11:53:49 -0400

    Add crazy first pass q-learning code

--- ai/reinforcement_learning.py
+++ ai/reinforcement_learning.py
@@ -1,29 +1,68 @@
+import numpy as np
+
+
 class QLearning:
     # TODO: Have a way to customize discrete buckets or something like that
-    def __init__(self, epsilon=0.01):
+    def __init__(self, epsilon=0.1, episodes=1000, learn_rate=0.1, discount=0.95):
         self.done = False
-        # TODO: Discretize? States and actions?
+        # TODO: Discretize? States and actions? Probably will end up being a numpy array
+        #   If discrete, a method for this is probably a good idea
         # The Q-table with every state-action value pair
-        self.table = None
+        self.q_table = np.zeros((1, 1))
         # Randomization of action selection
         self.epsilon = epsilon
+        self.epsilon_decay = None
+        # Amount of iterations
+        self.episodes = episodes
+        # Alpha in the bellman equation
+        self.learn_rate = learn_rate
+        # How important future actions are over current actions
+        self.discount = discount
 
-    def _environment(self):
-        pass
+    @staticmethod
+    def _environment(routed):
+        # TODO: Update environment (import simulator)
+        # For now, a positive one if routed, negative one if blocked
+        if routed:
+            reward = 1
+        else:
+            reward = -1
 
+        # TODO: return an actual new state here or somewhere else?
+        new_state = 1
+        return reward, new_state
+
+    def _setup_episode(self):
+        # Init q-table
+        self.q_table = np.zeros((1, 1))
+
+    # TODO: Save best q-table? Or something like that
+    # TODO: Training and testing (make a plan for all this, it's nonsense at the moment)
     def run(self):
-        pass
+        for episode in range(self.episodes):
+            self._setup_episode()
 
-    # TODO: Transfer this to the init, it is your setup
-    def setup(self):
-        # TODO: Update to a custom environment
-        env = None
-        # TODO: Define actions
-        actions = None
+            while not self.done:
+                state = None
 
-        while not self.done:
-            # TODO: Update, this will be my environment function I believe
-            new_state, reward, self.done = None
+                # Choose a random action
+                # TODO: Make sure this works
+                if np.random.randint(0, 100) > self.epsilon:
+                    # TODO: Make sure this works
+                    action = self.q_table[np.random.randint(np.size(self.q_table[0]))]
+                else:
+                    action = np.argmax(self.q_table[state])
+                reward, new_state = self._environment(routed=True)
+
+                max_future_q = np.max(self.q_table[new_state])
+                current_q = self.q_table[(state, action)]
+
+                # Bellman equation
+                new_q = ((1 - self.learn_rate) * current_q) + (
+                        self.learn_rate * (reward + self.discount * max_future_q))
+
+                # Update q-table
+                self.q_table[(state, action)] = new_q
 
 
 # TODO:
