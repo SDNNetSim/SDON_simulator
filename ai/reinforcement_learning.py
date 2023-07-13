04dc14f92d311eea54df5fff9627a426ce4e66f5
Ryan McCann <ryan_mccann@student.uml.edu>
2023-07-13 12:01:14 -0400

    Finish Q-learning code

--- ai/reinforcement_learning.py
+++ ai/reinforcement_learning.py
@@ -1,73 +1,143 @@
 import numpy as np
+import networkx as nx
 
 
 class QLearning:
-    # TODO: Have a way to customize discrete buckets or something like that
-    def __init__(self, epsilon=0.1, episodes=1000, learn_rate=0.1, discount=0.95):
-        self.done = False
-        # TODO: Discretize? States and actions? Probably will end up being a numpy array
-        #   If discrete, a method for this is probably a good idea
-        # The Q-table with every state-action value pair
-        self.q_table = np.zeros((1, 1))
-        # Randomization of action selection
+    """
+    Controls methods related to the Q-learning reinforcement learning algorithm.
+    """
+
+    def __init__(self, epsilon: float = 0.1, epsilon_decay: float = 0.01, episodes: int = 1000, learn_rate: float = 0.1,
+                 discount: float = 0.95):
+        """
+        Initializes the QLearning class.
+
+        :param epsilon: Parameter in the bellman equation to determine degree of randomness
+        :type epsilon: float
+
+        :param epsilon_decay: Determines the decay in the degree of randomness for every episode
+        :type epsilon_decay: float
+
+        :param episodes: The number of iterations or simulations
+        :type episodes: int
+
+        :param learn_rate: The learning rate, alpha, in the bellman equation
+        :type learn_rate: float
+
+        :param discount: The discount factor in the bellman equation to determine the balance between current and
+                         future rewards
+        """
+        # Contains all state and action value pairs
+        self.q_table = None
         self.epsilon = epsilon
-        self.epsilon_decay = None
-        # Amount of iterations
+        # TODO: Use epsilon decay
+        self.epsilon_decay = epsilon_decay
         self.episodes = episodes
-        # Alpha in the bellman equation
         self.learn_rate = learn_rate
-        # How important future actions are over current actions
         self.discount = discount
 
-    @staticmethod
-    def _environment(routed):
-        # TODO: Update environment (import simulator)
-        # For now, a positive one if routed, negative one if blocked
+        # Statistics used for plotting
+        self.rewards_dict = {'episode': [], 'average': [], 'min': [], 'max': [], 'rewards': []}
+
+    def plot_rewards(self):
+        """
+        Plots reward values vs. episodes.
+        """
+        pass
+
+    def environment(self, routed, path):
+        """
+        The custom environment that updates the Q-table with respect to a reward policy.
+
+        :param routed: If a request was successfully routed or not
+        :type routed: bool
+
+        :param path: The path taken from source to destination
+        :type path: list
+
+        :return: The reward value
+        :rtype: int
+        """
         if routed:
-            reward = 1
+            reward = 1.0
         else:
-            reward = -1
+            reward = -1.0
 
-        # TODO: return an actual new state here or somewhere else?
-        new_state = 1
-        return reward, new_state
+        for i in range(len(path) - 1):
+            state = path[i]
+            new_state = path[i + 1]
 
-    def _setup_episode(self):
-        # Init q-table
-        self.q_table = np.zeros((1, 1))
-
-    # TODO: Save best q-table? Or something like that
-    # TODO: Training and testing (make a plan for all this, it's nonsense at the moment)
-    def run(self):
-        for episode in range(self.episodes):
-            self._setup_episode()
-
-            while not self.done:
-                state = None
-
-                # Choose a random action
-                # TODO: Make sure this works
-                if np.random.randint(0, 100) > self.epsilon:
-                    # TODO: Make sure this works
-                    action = self.q_table[np.random.randint(np.size(self.q_table[0]))]
-                else:
-                    action = np.argmax(self.q_table[state])
-                reward, new_state = self._environment(routed=True)
-
+            # If terminating node, only consider the reward (there is no future state)
+            if i + 1 == len(path):
+                max_future_q = reward
+            else:
                 max_future_q = np.max(self.q_table[new_state])
-                current_q = self.q_table[(state, action)]
 
-                # Bellman equation
-                new_q = ((1 - self.learn_rate) * current_q) + (
-                        self.learn_rate * (reward + self.discount * max_future_q))
+            current_q = self.q_table[(state, new_state)]
+            new_q = ((1.0 - self.learn_rate) * current_q) + (
+                    self.learn_rate * (reward + self.discount * max_future_q))
 
-                # Update q-table
-                self.q_table[(state, action)] = new_q
+            self.q_table[(state, new_state)] = new_q
+
+    def setup_episode(self, seed, topology):
+        """
+        Initializes a single episode, which is synonymous with one simulation.
+
+        :param seed: The random seed
+        :type seed: int
+
+        :param topology: The network topology
+        :type topology: nx.Graph
+        """
+        np.random.seed(seed)
+        # Init q-table for USNet, a 24 node network
+        self.q_table = np.zeros(24, 24)
+
+        for source in range(0, 24):
+            for destination in range(0, 24):
+                if source == destination:
+                    self.q_table[(source, destination)] = np.nan
+                    continue
+
+                # A link exists between these nodes, init to zero
+                if source in topology.neighbors(destination):
+                    self.q_table[(source, destination)] = 0
+                else:
+                    self.q_table[(source, destination)] = np.nan
+
+    # TODO: Save the results and the q-table after every simulation?
+    def route(self, source, destination):
+        """
+        Determines a route from source to destination using Q-Learning.
+
+        :param source: The source node
+        :type source: int
+
+        :param destination: The destination node
+        :type destination: int
+
+        :return: The path from source to destination
+        :rtype: list
+        """
+        path = [source]
+        while True:
+            random_float = np.random.random_sample()
+            while True:
+                # Choose a random action with respect to epsilon
+                if random_float < self.epsilon:
+                    next_node = self.q_table[np.random.randint(24)]
+                else:
+                    next_node = np.argmax(self.q_table[source])
+
+                q_value = self.q_table[(source, next_node)]
+                # No connection exists between these nodes
+                if np.isnan(q_value):
+                    continue
+
+                path.append(next_node)
+                if next_node == destination:
+                    return path
 
 
-# TODO:
-#  - Implement examples and run (simple implementation & custom environment)
-#  - Create a plan for your q-table and environment
-#  - Present plan/Begin implementing
 if __name__ == '__main__':
     pass
