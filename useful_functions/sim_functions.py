fcdfdfb606289ef218b3a4020ddf303f0273a541
Ryan McCann <ryan_mccann@student.uml.edu>
2023-07-21 16:00:38 -0400

    Update sim_functions.py for AI integration

--- useful_functions/sim_functions.py
+++ useful_functions/sim_functions.py
@@ -4,6 +4,14 @@ from typing import List
 # Third-party library imports
 import networkx as nx
 
+# Local application imports
+from ai.reinforcement_learning import *
+
+# TODO: I don't like this at all, objectify these functions
+# TODO: Update doc strings
+# A class dedicated to artificial intelligence routing and spectrum assignment
+AI_OBJ = None
+
 
 def get_path_mod(mod_formats: dict, path_len: int):
     """
@@ -61,3 +69,48 @@ def find_path_len(path: List[int], topology: nx.Graph):
         path_len += topology[path[i]][path[i + 1]]['length']
 
     return path_len
+
+
+# TODO: Finish
+def save_ai_obj(**kwargs):
+    global AI_OBJ
+
+    if kwargs['sim_data']['ai_algorithm'] == 'q_learning':
+        if kwargs['sim_data']['is_training']:
+            pass
+        else:
+            pass
+
+
+def update_ai_obj(**kwargs):
+    global AI_OBJ
+
+    if kwargs['sim_data']['ai_algorithm'] == 'q_learning':
+        if kwargs['update_env']:
+            AI_OBJ.update_environment(routed=kwargs['routed'], path=kwargs['path'], free_slots=kwargs['free_slots'])
+        else:
+            # Decay epsilon for half of the iterations evenly each time
+            if 1 <= kwargs['iteration'] <= kwargs['iteration'] // 2 and kwargs['sim_data']['is_training']:
+                decay_amount = (AI_OBJ.epsilon / (kwargs['iteration'] // 2) - 1)
+                AI_OBJ.decay_epsilon(amount=decay_amount)
+
+
+def setup_ai_obj(**kwargs):
+    global AI_OBJ
+
+    if kwargs['sim_data']['ai_algorithm'] == 'q_learning':
+        AI_OBJ = QLearning()
+        AI_OBJ.topology = kwargs['topology']
+        AI_OBJ.setup_environment()
+
+        if not kwargs['sim_data']['is_training']:
+            AI_OBJ.load_table(path=kwargs['sim_info'], max_segments=kwargs['max_segments'])
+        else:
+            AI_OBJ.load_table(path=kwargs['sim_data']['trained_table'], max_segments=kwargs['max_segments'])
+
+        AI_OBJ.set_seed(seed=kwargs['seed'])
+        # Spectrum assignment for AI not supported at this time
+        return AI_OBJ.route, None
+
+    else:
+        raise NotImplementedError(f"The {kwargs['sim_data']['ai_algorithm']} ai algorithm is not recognized.")
