817a2467f0610eafbfc582d8ea50b6f53e8e36fb
Ryan McCann <ryan_mccann@student.uml.edu>
2023-07-24 12:31:35 -0400

    Objectify AIMethods in sim_functions.py

--- useful_functions/sim_functions.py
+++ useful_functions/sim_functions.py
@@ -7,11 +7,6 @@ import networkx as nx
 # Local application imports
 from ai.reinforcement_learning import *
 
-# TODO: I don't like this at all, objectify these functions
-# TODO: Update doc strings
-# A class dedicated to artificial intelligence routing and spectrum assignment
-AI_OBJ = None
-
 
 def get_path_mod(mod_formats: dict, path_len: int):
     """
@@ -71,46 +66,139 @@ def find_path_len(path: List[int], topology: nx.Graph):
     return path_len
 
 
-# TODO: Finish
-def save_ai_obj(**kwargs):
-    global AI_OBJ
+class AIMethods:
+    """
+    Contains methods related to artificial intelligence for the simulator.
+    """
 
-    if kwargs['sim_data']['ai_algorithm'] == 'q_learning':
-        if kwargs['sim_data']['is_training']:
-            pass
+    def __init__(self, algorithm: str, sim_info: str, max_segments: int, is_training: bool, topology: nx.Graph,
+                 seed: int):
+        """
+        Initializes the AIMethods class.
+
+        :param algorithm: The type of AI algorithm to be used in the simulation.
+        :type algorithm: str
+
+        :param sim_info: Relevant information to the current running simulation.
+        :type sim_info: str
+
+        :param max_segments: The maximum allowed segments allowed for a single request.
+        :type max_segments: int
+
+        :param is_training: Determines if the simulation is training or testing.
+        :type is_training: bool
+
+        :param topology: The network topology.
+        :type topology: nx.Graph
+
+        :param seed: The seed used for random generation.
+        :type seed: int
+        """
+        self.algorithm = algorithm
+        self.sim_info = sim_info
+        self.max_segments = max_segments
+        self.is_training = is_training
+        self.topology = topology
+        self.seed = seed
+
+        # An object for the chosen AI algorithm
+        self.ai_obj = None
+
+    def q_save(self, path: str, max_segments: int):
+        """
+        Saves the current state of the Q-table.
+
+        :param path: The path to save the table
+        :type path: str
+
+        :param max_segments: The maximum number of segments allowed.
+        :type max_segments: int
+        """
+        self.ai_obj.save_table(path=path, max_segments=max_segments)
+
+    def q_update_env(self, routed: bool, path: list, free_slots: int, iteration: int):
+        """
+        Updates the Q-learning environment.
+
+        :param routed: A flag to determine if a request was routed or not.
+        :type routed: bool
+
+        :param path: The path for the request.
+        :type path: list
+
+        :param free_slots: The number of total free slots in the path.
+        :type free_slots: int
+
+        :param iteration: The current iteration of the simulation.
+        :type iteration: int
+        """
+        self.ai_obj.update_environment(routed=routed, path=path, free_slots=free_slots)
+
+        # Decay epsilon for half of the iterations evenly each time
+        if 1 <= iteration <= iteration // 2 and self.is_training:
+            decay_amount = (self.ai_obj.epsilon / (iteration // 2) - 1)
+            self.ai_obj.decay_epsilon(amount=decay_amount)
+
+    def q_spectrum(self):
+        raise NotImplementedError
+
+    def q_routing(self, source: int, destination: int, mod_formats: dict):
+        """
+        Given a request, determines the path from source to destination.
+
+        :param source: The source node.
+        :type source: int
+
+        :param destination: The destination node.
+        :type destination: int
+
+        :param mod_formats: Modulation format info related to the size of the request (in Gbps).
+        :type mod_formats: dict
+
+        :return: A path from source to destination.
+        :rtype: list
+        """
+        path = self.ai_obj.route(source=source, destination=destination, mod_formats=mod_formats)
+        return path
+
+    def init_q_learning(self, epsilon: float = 0.2, episodes: int = 1000, learn_rate: float = 0.5,
+                        discount: float = 0.2, trained_table: str = None):
+        """
+        Initializes a QLearning class and sets up the initial environment and Q-table.
+
+        :param epsilon: Parameter in the bellman equation to determine degree of randomness.
+        :type epsilon: float
+
+        :param episodes: The number of iterations or simulations.
+        :type episodes: int
+
+        :param learn_rate: The learning rate, alpha, in the bellman equation.
+        :type learn_rate: float
+
+        :param discount: The discount factor in the bellman equation to determine the balance between current and
+                         future rewards.
+        :type discount: float
+
+        :param trained_table: A path to an already trained Q-table.
+        :type trained_table: str
+        """
+        self.ai_obj = QLearning(epsilon=epsilon, episodes=episodes, learn_rate=learn_rate, discount=discount,
+                                topology=self.topology)
+        self.ai_obj.setup_environment()
+
+        # Load a pre-trained table or start a new one
+        if self.is_training:
+            self.ai_obj.load_table(path=self.sim_info, max_segments=self.max_segments)
         else:
-            pass
+            self.ai_obj.load_table(path=trained_table, max_segments=self.max_segments)
 
+        self.ai_obj.set_seed(self.seed)
 
-def update_ai_obj(**kwargs):
-    global AI_OBJ
-
-    if kwargs['sim_data']['ai_algorithm'] == 'q_learning':
-        if kwargs['update_env']:
-            AI_OBJ.update_environment(routed=kwargs['routed'], path=kwargs['path'], free_slots=kwargs['free_slots'])
+    def setup(self):
+        """
+        Responsible for setting up available AI algorithms and their methods.
+        """
+        if self.algorithm == 'q_learning':
+            self.init_q_learning()
         else:
-            # Decay epsilon for half of the iterations evenly each time
-            if 1 <= kwargs['iteration'] <= kwargs['iteration'] // 2 and kwargs['sim_data']['is_training']:
-                decay_amount = (AI_OBJ.epsilon / (kwargs['iteration'] // 2) - 1)
-                AI_OBJ.decay_epsilon(amount=decay_amount)
-
-
-def setup_ai_obj(**kwargs):
-    global AI_OBJ
-
-    if kwargs['sim_data']['ai_algorithm'] == 'q_learning':
-        AI_OBJ = QLearning()
-        AI_OBJ.topology = kwargs['topology']
-        AI_OBJ.setup_environment()
-
-        if not kwargs['sim_data']['is_training']:
-            AI_OBJ.load_table(path=kwargs['sim_info'], max_segments=kwargs['max_segments'])
-        else:
-            AI_OBJ.load_table(path=kwargs['sim_data']['trained_table'], max_segments=kwargs['max_segments'])
-
-        AI_OBJ.set_seed(seed=kwargs['seed'])
-        # Spectrum assignment for AI not supported at this time
-        return AI_OBJ.route, None
-
-    else:
-        raise NotImplementedError(f"The {kwargs['sim_data']['ai_algorithm']} ai algorithm is not recognized.")
+            raise NotImplementedError(f'Algorithm: {self.algorithm} not recognized.')
