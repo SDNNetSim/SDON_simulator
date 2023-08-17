a17b36488e26b267c3fd640a79e4844b3ffae49f
Ryan McCann <ryan_mccann@student.uml.edu>
2023-08-17 15:10:51 -0400

    Create example environment

--- ai/envs/first_env.py
+++ ai/envs/first_env.py
@@ -4,36 +4,50 @@ from gymnasium import spaces
 
 
 class CustomEnv(gym.Env):
-    # TODO: Change
-    metadata = {"render_modes": ["human"], "render_fps": 30}
+    LEFT = 0
+    RIGHT = 1
 
     def __init__(self):
-        super().__init__()
+        # TODO: What?
+        super(CustomEnv, self).__init__()
 
-        discrete_actions = 10
+        discrete_actions = 2
+        self.agent_value = 0
         self.action_space = spaces.Discrete(discrete_actions)
 
-        channels = 5
-        height = 1
-        width = 20
-        self.observation_space = spaces.Box(low=0, high=255,
-                                            shape=(channels, height, width), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0, high=100, shape=(1,), dtype=np.uint8)
 
     def step(self, action):
-        observation = None
-        reward = None
-        terminated = None
+        if action == self.LEFT:
+            self.agent_value += 1
+            reward = 1
+        elif action == self.RIGHT:
+            self.agent_value -= 1
+            reward = 0
+        else:
+            raise ValueError('Received an invalid action.')
+
+        if self.agent_value == 100:
+            terminated = True
+        else:
+            terminated = False
+
+        observation = self.agent_value
+        # Unused variables for now but suggested to have per the documentation
         truncated = None
         info = None
 
         return observation, reward, terminated, truncated, info
 
     def reset(self, seed=None, options=None):
-        observation = None
+        self.agent_value = 0
+
+        observation = self.agent_value
         info = None
 
         return observation, info
 
+    # TODO: Probably not needed these methods since we won't use a GUI
     def render(self):
         raise NotImplementedError
 
