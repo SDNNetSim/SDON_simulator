ceb78dfc3d5cc36f83aac5df99587f1d417a5650
Ryan McCann <ryan_mccann@student.uml.edu>
2023-08-22 18:34:24 -0400

    Update config_constants.py for new ai args

--- config/config_constants.py
+++ config/config_constants.py
@@ -55,7 +55,6 @@ ARASH_REQUIRED_OPTIONS = {
 OTHER_OPTIONS = {
     'ai_algorithm': str,
     'ai_arguments': ast.literal_eval,
-    'is_training': str_to_bool,
     'seeds': list,
     'beta': float,
     'train_file': str,
