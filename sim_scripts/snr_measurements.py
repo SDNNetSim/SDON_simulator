e2f67e7fa1656d611e89a9522b9cb497747185a8
arashr88 <rezaee.arash88@gmail.com>
2023-08-23 17:55:35 -0400

    add XT_calculator function and update XCI function

--- sim_scripts/snr_measurements.py
+++ sim_scripts/snr_measurements.py
@@ -89,7 +89,9 @@ class SnrMeasurments:
         return G_SCI
     
     
-    def _XCI_calculator(self, Fi, link, PSDi, BW, visited_channel):
+    def _XCI_calculator(self, Fi, link):
+        visited_channel = []
+        MCI = 0
         for w in range(self.spectral_slots):
             if self.network_spec_db[(self.path[link], self.path[link+1])]['cores_matrix'][self.SP['core_num']][w] > 0: #!= 0 :
                 if self.network_spec_db[(self.path[link], self.path[link+1])]['cores_matrix'][self.SP['core_num']][w] in visited_channel:
@@ -102,7 +104,15 @@ class SnrMeasurments:
                 PSDj = self.input_power / BWj
                 if Fi != Fj:
                     MCI = MCI + ((PSDj ** 2) * math.log( abs((abs(Fi-Fj)+(BWj/2))/(abs(Fi-Fj)-(BWj/2)))))
-        
+        return MCI, visited_channel
+    
+    def _XT_calculator(self, link_id, length):
+        XT_lambda = (2 * self.physical_topology['links'][link_id]['fiber']["bending_radius"] * self.physical_topology['links'][link_id]['fiber']["mode_coupling_co"] ** 2) / (self.physical_topology['links'][link_id]['fiber']["propagation_const"] * self.physical_topology['links'][link_id]['fiber']["core_pitch"])
+        no_adjacent_core = 6
+        lng = length*10*10**3
+        XT_calc = (no_adjacent_core * ( 1 - math.exp(-(no_adjacent_core+1)*2*XT_lambda*lng))) / (1 + no_adjacent_core * math.exp(-(no_adjacent_core+1)*2*XT_lambda*lng))
+        P_XT = no_adjacent_core * XT_lambda * length * 10**3 * self.input_power  
+        return  P_XT  
     def SNR_check_NLI_ASE_XT(self):
         
         light_frequncy = (1.9341 * 10 ** 14)
@@ -113,19 +123,22 @@ class SnrMeasurments:
         
         PSD_NLI = 0
         PSD_corr = 0
+        SNR = 0 
         for link in range(0, len(self.path)-1):
             MCI = 0
             Num_span = 0
-            visited_channel = []
+            #visited_channel = []
             link_id = self.network_spec_db[(self.path[link], self.path[link+1])]['link_num']
             taken_channels = self._find_taken_channels((self.path[link], self.path[link+1]))
+            if len(taken_channels) > 0:
+                print("5")
             Mio = ( 3 * ( self.physical_topology['links'][link_id]['fiber']['non_linearity'] ** 2 ) ) / ( 2 * math.pi * self.physical_topology['links'][link_id]['fiber']['attenuation'] * np.abs( self.physical_topology['links'][link_id]['fiber']['dispersion'] ))
             G_SCI = self._SCI_calculator(link_id, PSDi, BW)
-            G_XCI = self._XCI_calculator()
-            length = 100 # TODO: chang based on input : length
+            G_XCI, visited_channel = self._XCI_calculator( Fi, link)
+            length = self.physical_topology['links'][link_id]['span_length']
             nsp = 1.8 # TODO self.physical_topology['links'][link_id]['fiber']['nsp']
-            bending_radius = 0.05 #TODO: self.physical_topology['links'][link_id]['fiber']["bending_radius"] 
-            
+            bending_radius = 0.05 # TODO: self.physical_topology['links'][link_id]['fiber']["bending_radius"] 
+            Num_span = self.physical_topology['links'][link_id]['length'] / length
             
             if self.phi:
                 hn = 0
@@ -139,41 +152,38 @@ class SnrMeasurments:
             
             PSD_ASE = 0
             if self.EGN:
-                PSD_NLI = ( ( ( G_SCI + MCI ) * Mio * PSDi) ) - PSD_corr
+                PSD_NLI = ( ( ( G_SCI + G_XCI ) * Mio * PSDi) ) - PSD_corr
             else:
-                PSD_NLI = ( ( ( G_SCI + MCI ) * Mio * PSDi) )
+                PSD_NLI = ( ( ( G_SCI + G_XCI ) * Mio * PSDi) )
             PSD_ASE = ( self.plank * light_frequncy * nsp ) * ( math.e ** (self.physical_topology['links'][link_id]['fiber']['attenuation'] * length * 10 ** 3 ) - 1 )
-            
-            
-            XT_lambda = (2 * self.physical_topology['links'][link_id]['fiber']["bending_radius"] * self.physical_topology['links'][link_id]['fiber']["mode_coupling_co"] ** 2) / (self.physical_topology['links'][link_id]['fiber']["propagation_const"] * self.physical_topology['links'][link_id]['fiber']["core_pitch"])
-            no_adjacent_core = 6
-            lng = length*10*10**3
-            XT_calc = (no_adjacent_core * ( 1 - math.exp(-(no_adjacent_core+1)*2*XT_lambda*lng))) / (1 + no_adjacent_core * math.exp(-(no_adjacent_core+1)*2*XT_lambda*lng))
-            P_XT = no_adjacent_core * XT_lambda * length * 10**3 * self.input_power 
+            SNR +=( 1 / ( PSDi / ( ( PSD_ASE + PSD_NLI ) * Num_span ) ) )
+            """
+
+            """
             #for i in range(1,100):
             #P_XT2 = self.input_power * math.exp(-)
-            SNR =( 1 / ( PSDi / ( ( PSD_ASE + PSD_NLI ) * Num_span ) ) )
+        SNR = 10 * math.log10( 1 / SNR ) 
+        print(SNR)
+        return True if SNR > self.requested_SNR else False
 
-            return True if SNR > self.requested_SNR else False
-
-            """
-            for i in range(1,100):
-                Num_span =  i
-                lng2 = Num_span * length 
-                P_XT2 = no_adjacent_core * XT_lambda * self.input_power * math.exp(-self.physical_topology['links'][link_id]['fiber']['attenuation'] * lng2) * lng2 * 10**3
-                P_XT2 = P_XT2 * self.no_assigned_slots
-                SNR = ( 1 / ( PSDi*BW / ( ( PSD_ASE*BW + PSD_NLI*BW ) * Num_span + P_XT2 ) ) )
-                SNR2 = 10*math.log10(1/SNR) 
-                if self.modulation_format == '64-QAM':
-                    snr_tr = 22#13.5
-                elif self.modulation_format == '16-QAM':
-                    snr_tr = 16 #9.5
-                elif self.modulation_format == 'QPSK':
-                    snr_tr = 7.5
-                if SNR2 < snr_tr:
-                    print( "Maximum distance:  " , (i-1) * length )
-                    break
-            """
+        """
+        for i in range(1,100):
+            Num_span =  i
+            lng2 = Num_span * length 
+            P_XT2 = no_adjacent_core * XT_lambda * self.input_power * math.exp(-self.physical_topology['links'][link_id]['fiber']['attenuation'] * lng2) * lng2 * 10**3
+            P_XT2 = P_XT2 * self.no_assigned_slots
+            SNR = ( 1 / ( PSDi*BW / ( ( PSD_ASE*BW + PSD_NLI*BW ) * Num_span + P_XT2 ) ) )
+            SNR2 = 10*math.log10(1/SNR) 
+            if self.modulation_format == '64-QAM':
+                snr_tr = 22#13.5
+            elif self.modulation_format == '16-QAM':
+                snr_tr = 16 #9.5
+            elif self.modulation_format == 'QPSK':
+                snr_tr = 7.5
+            if SNR2 < snr_tr:
+                print( "Maximum distance:  " , (i-1) * length )
+                break
+        """
                 
 
 
