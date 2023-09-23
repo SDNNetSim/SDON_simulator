3ff65dc3768a9b29157cbb6378aa628f2cd11b16
Ryan McCann <ryan_mccann@student.uml.edu>
2023-09-23 16:33:35 -0400

    Fix beta params bug
    Array as a string was passing to run_sim.py

--- bash_scripts/run_sim.sh
+++ bash_scripts/run_sim.sh
@@ -3,8 +3,8 @@
 #SBATCH -p cpu-long
 #SBATCH -c 16
 #SBATCH --mem=16000
-#SBATCH -t 4-00:00:00
-#SBATCH -o slurm-%j.out
+#SBATCH -t 0-01:00:00
+#SBATCH -o /slurm/slurm-%j.out
 
 # shellcheck disable=SC2164
 cd
@@ -13,4 +13,4 @@ cd /work/pi_vinod_vokkarane_uml_edu/git/SDN_Simulator/
 
 beta_array=("0.000001" "0.1")
 
-python run_sim.py --beta beta_array[$SLURM_ARRAY_TASK_ID]
+python run_sim.py --beta ${beta_array[$SLURM_ARRAY_TASK_ID]}
