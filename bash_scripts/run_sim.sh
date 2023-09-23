79a0dedd3a164b6ba79503e623b916004edd0e1b
Ryan McCann <ryan_mccann@student.uml.edu>
2023-09-23 16:09:16 -0400

    Update run_sim.sh for array jobs
    Referring to Unity cluster slurm jobs specifically

--- bash_scripts/run_sim.sh
+++ bash_scripts/run_sim.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 #SBATCH -p cpu-long
-#SBATCH -c 20
+#SBATCH -c 16
 #SBATCH --mem=16000
-#SBATCH -t 10-00:00:00
+#SBATCH -t 4-00:00:00
 #SBATCH -o slurm-%j.out
 
+# shellcheck disable=SC2164
 cd
+# shellcheck disable=SC2164
 cd /work/pi_vinod_vokkarane_uml_edu/git/SDN_Simulator/
-python run_sim.py
+
+cat <<-PYTHONSCRIPT >mypythonscript.py
+    #!/usr/bin/env python3
+    import sys
+    job_names = ["job1", "job2"]
+    job_index = int(sys.args[1])
+    print(job_names[job_index])
+PYTHONSCRIPT
+
+python run_sim.py $SLURM_ARRAY_TASK_ID
