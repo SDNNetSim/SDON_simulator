9e61ed132e5ee9a87419013cbee81b7bcf9c8d68
Ryan McCann <ryan_mccann@student.uml.edu>
2023-10-04 17:31:33 +0000

    Improve run_sim.sh

--- bash_scripts/run_sim.sh
+++ bash_scripts/run_sim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH -p cpu-long
-#SBATCH -c 16
+#SBATCH -c 28
 #SBATCH --mem=16000
-#SBATCH -t 0-01:00:00
+#SBATCH -t 3-00:00:00
 #SBATCH -o slurm-%j.out
 
 # shellcheck disable=SC2164
@@ -11,6 +11,7 @@ cd
 # shellcheck disable=SC2164
 cd /work/pi_vinod_vokkarane_uml_edu/git/SDN_Simulator/
 
-beta_array=("0.000001" "0.1")
+beta_array=("0.000001" "0.1" "0.2" "0.4" "0.6" "0.8")
+k_array=("1" "2" "3" "4" "5")
 
-python run_sim.py --beta ${beta_array[$SLURM_ARRAY_TASK_ID]}
+python run_sim.py --route_method xt_aware --num_requests 10000 --beta ${beta_array[$SLURM_ARRAY_TASK_ID]}
