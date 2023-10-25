7f8bea58086a30fed09a43b89df2e005bd49752d
arashr88 <rezaee.arash88@gmail.com>
2023-10-25 01:02:19 -0400

    update

--- bash_scripts/run_sim.sh
+++ bash_scripts/run_sim.sh
@@ -14,4 +14,4 @@ cd /work/pi_vinod_vokkarane_uml_edu/git/SDN_Simulator/
 beta_array=("0.000001" "0.1" "0.2" "0.4" "0.6" "0.8")
 k_array=("1" "2" "3" "4" "5")
 
-python run_sim.py --route_method k_shortest_path --spectral_slots 256 --allocation_method prioritized_first_fit --num_requests 25000 --k_paths ${k_array[$SLURM_ARRAY_TASK_ID]}
+python run_sim.py --route_method xt_aware --spectral_slots 256 --allocation_method prioritized_first_fit --num_requests 25000 --beta ${beta_array[$SLURM_ARRAY_TASK_ID]}
