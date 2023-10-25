3743b6c199a422ad42f3f20969b458c3f9e4182f
arashr88 <rezaee.arash88@gmail.com>
2023-10-25 00:58:04 -0400

    update

--- bash_scripts/run_sim.sh
+++ bash_scripts/run_sim.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -p cpu-long
 #SBATCH -c 28
-#SBATCH --mem=16000
-#SBATCH -t 2-00:00:00
+#SBATCH --mem=48000
+#SBATCH -t 36-00:00:00
 #SBATCH -o slurm-%j.out
 
 # shellcheck disable=SC2164
@@ -14,4 +14,4 @@ cd /work/pi_vinod_vokkarane_uml_edu/git/SDN_Simulator/
 beta_array=("0.000001" "0.1" "0.2" "0.4" "0.6" "0.8")
 k_array=("1" "2" "3" "4" "5")
 
-python run_sim.py --route_method k_shortest_path --spectral_slots 256 --allocation_method prioritized_first_fit --num_requests 25000 --k_paths ${beta_array[$SLURM_ARRAY_TASK_ID]}
+python run_sim.py --route_method k_shortest_path --spectral_slots 256 --allocation_method prioritized_first_fit --num_requests 25000 --k_paths ${k_array[$SLURM_ARRAY_TASK_ID]}
