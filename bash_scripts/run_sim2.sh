6d233599b9efdc3dcf9aab462aa3eec7f6fdf55b
arashr88 <rezaee.arash88@gmail.com>
2023-11-02 19:47:51 -0400

    update bash scripts

--- bash_scripts/run_sim2.sh
+++ bash_scripts/run_sim2.sh
@@ -15,4 +15,4 @@ cd /home/arash_rezaee_student_uml_edu/Git/SDN_Simulator/
 beta_array=("0.000001" "0.1" "0.2" "0.4" "0.6" "0.8")
 k_array=("1" "2" "3" "4" "5")
 
-python run_sim.py --sim_type arash --route_method xt_aware --k_paths ${k_array[$SLURM_ARRAY_TASK_ID]}
+python run_sim.py --sim_type arash --route_method k_shortest_path --k_paths ${k_array[$SLURM_ARRAY_TASK_ID]}
