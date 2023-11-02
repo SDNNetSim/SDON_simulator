1e2c1ef0077d6b99d0bc5d8d4e5ad99e0502816a
arashr88 <rezaee.arash88@gmail.com>
2023-11-02 17:05:57 -0400

    new scripts

---
+++ bash_scripts/run_sim2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -p cpu-long
+#SBATCH -c 28
+#SBATCH --mem=48000
+#SBATCH -t 1-12
+#SBATCH -o slurm-%j.out
+
+# shellcheck disable=SC2164
+cd
+# shellcheck disable=SC2164
+cd /home/arash_rezaee_student_uml_edu/Git/SDN_Simulator/
+
+
+beta_array=("0.000001" "0.1" "0.2" "0.4" "0.6" "0.8")
+k_array=("1" "2" "3" "4" "5")
+
+python run_sim.py --sim_type arash --route_method xt_aware --k_paths ${k_array[$SLURM_ARRAY_TASK_ID]}
---
+++ bash_scripts/run_sim3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -p cpu-long
+#SBATCH -c 28
+#SBATCH --mem=48000
+#SBATCH -t 1-12
+#SBATCH -o slurm-%j.out
+
+# shellcheck disable=SC2164
+cd
+# shellcheck disable=SC2164
+cd /home/arash_rezaee_student_uml_edu/Git/SDN_Simulator/
+
+
+
+python run_sim.py --sim_type arash --route_method xt_aware --xt_type without_length
