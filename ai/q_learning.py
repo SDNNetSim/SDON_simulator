5ecc8080ab9f39e76d53a395f3494a6d0e6ce0c2
Ryan McCann <ryan_mccann@student.uml.edu>
2023-11-03 16:35:42 -0400

    Add another baseline policy

--- ai/q_learning.py
+++ ai/q_learning.py
@@ -67,6 +67,7 @@ class QLearning:
             'policy_one': self._get_policy_one,
             'policy_two': self._get_policy_two,
             'policy_three': self._get_policy_three,
+            'policy_four': self._get_policy_four,
         }
         # Simulation methods related to routing
         self.routing_obj = Routing(beta=properties['beta'], topology=properties['topology'],
@@ -193,6 +194,15 @@ class QLearning:
         self.ai_arguments['learn_rate'] = properties_obj['learn_rate']
         self.ai_arguments['discount'] = properties_obj['discount_factor']
         self.rewards_dict = properties_obj['reward_info']
+
+    @staticmethod
+    def _get_policy_four(routed: bool):
+        if routed:
+            resp = 10.0
+        else:
+            resp = -1.0
+
+        return resp
 
     @staticmethod
     def _get_policy_three(routed: bool):
