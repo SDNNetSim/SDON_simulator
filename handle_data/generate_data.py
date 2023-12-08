64578c52d813078e6dd574e07695fad681b3c5fb
Ryan McCann <ryan_mccann@student.uml.edu>
2023-12-08 12:47:12 -0500

    Update generated data
    - Use a data constants file instead
    - Minor updates to docstrings
    - Small efficiency updates

--- handle_data/generate_data.py
+++ handle_data/generate_data.py
@@ -1,17 +1,18 @@
 import math
+from .data_constants import YUE_REACH_ASSUMPTIONS, ARASH_REACH_ASSUMPTIONS
 
 
 def create_pt(cores_per_link: int, network_data: dict):
     """
     Generates information relevant to the physical topology of the network.
 
-    :param cores_per_link: The number of cores in each fiber link
+    :param cores_per_link: The number of independent cores in each optical fiber.
     :type cores_per_link: int
-    :param network_data: A dictionary mapping tuples of source and destination nodes to the length of the corresponding
-                        link
+
+    :param network_data: A mapping of source and destination nodes to their lengths.
     :type network_data: dict
 
-    :return: A dictionary containing information for the physical topology of the network
+    :return: Information for the physical topology of the network.
     :rtype: dict
     """
     # Fiber properties that apply to all fibers in a link
@@ -20,7 +21,7 @@ def create_pt(cores_per_link: int, network_data: dict):
         'non_linearity': 1.3e-3,
         'dispersion': (16e-6 * 1550e-9 ** 2) / (2 * math.pi * 3e8),
         'num_cores': cores_per_link,
-        'fiber_type': 0,  # TODO: Is this always supposed to be 0? Add a comment explaining why if so.
+        'fiber_type': 0,
         'bending_radius': 0.05,
         'mode_coupling_co': 4.0e-4,
         'propagation_const': 4e6,
@@ -45,107 +46,19 @@ def create_pt(cores_per_link: int, network_data: dict):
     return physical_topology
 
 
-def create_bw_info(sim_type: str = None):
+def create_bw_info(sim_type: str):
     """
-    Determines the number of spectral slots needed for every modulation format in each bandwidth.
+    Determines the number of spectral slots needed and allowed reach for every modulation format and bandwidth.
 
-    :param sim_type: The type of simulation to perform (either 'yue' or 'arash').
-    :type sim_type: str or None
+    :param sim_type: The type of simulation to perform.
+    :type sim_type: str
 
-    :return: The number of spectral slots needed for each bandwidth and modulation format pair.
+    :return: The number of spectral slots needed and reach allowed for each bandwidth and modulation format pair.
+    :rtype: dict
     """
-    # Check if sim_type is valid
-    if sim_type not in ['yue', 'arash', None]:
-        raise NotImplementedError(f"Invalid simulation type '{sim_type}'")
-
-    bw_info = dict()
-
-    # fill the template with maximum lengths for the given bandwidth
     if sim_type == 'yue':
-        bw_info.update({
-            '25': {'QPSK': {'max_length': 22160, 'slots_needed': 1}, '16-QAM': {'max_length': 9500, 'slots_needed': 1},
-                   '64-QAM': {'max_length': 3664, 'slots_needed': 1}},
-            '50': {'QPSK': {'max_length': 11080, 'slots_needed': 2}, '16-QAM': {'max_length': 4750, 'slots_needed': 1},
-                   '64-QAM': {'max_length': 1832, 'slots_needed': 1}},
-            '100': {'QPSK': {'max_length': 5540, 'slots_needed': 4}, '16-QAM': {'max_length': 2375, 'slots_needed': 2},
-                    '64-QAM': {'max_length': 916, 'slots_needed': 2}},
-            '200': {'QPSK': {'max_length': 2770, 'slots_needed': 8}, '16-QAM': {'max_length': 1187, 'slots_needed': 4},
-                    '64-QAM': {'max_length': 458, 'slots_needed': 3}},
-            '400': {'QPSK': {'max_length': 1385, 'slots_needed': 16}, '16-QAM': {'max_length': 594, 'slots_needed': 8},
-                    '64-QAM': {'max_length': 229, 'slots_needed': 6}},
-        })
-    elif sim_type == 'arash':
-        bw_info.update({
-                        "25": {
-                            "QPSK": {
-                                "max_length": 20759,
-                                "slots_needed": 1
-                            },
-                            "16-QAM": {
-                                "max_length": 9295,
-                                "slots_needed": 1
-                            },
-                            "64-QAM": {
-                                "max_length": 3503,
-                                "slots_needed": 1
-                            }
-                        },
-                        "50": {
-                            "QPSK": {
-                                "max_length": 10380,
-                                "slots_needed": 2
-                            },
-                            "16-QAM": {
-                                "max_length": 4648,
-                                "slots_needed": 1
-                            },
-                            "64-QAM": {
-                                "max_length": 1752,
-                                "slots_needed": 1
-                            }
-                        },
-                        "100": {
-                            "QPSK": {
-                                "max_length": 5190,
-                                "slots_needed": 3
-                            },
-                            "16-QAM": {
-                                "max_length": 2324,
-                                "slots_needed": 2
-                            },
-                            "64-QAM": {
-                                "max_length": 876,
-                                "slots_needed": 1
-                            }
-                        },
-                        "200": {
-                            "QPSK": {
-                                "max_length": 2595,
-                                "slots_needed": 5
-                            },
-                            "16-QAM": {
-                                "max_length": 1162,
-                                "slots_needed": 3
-                            },
-                            "64-QAM": {
-                                "max_length": 438,
-                                "slots_needed": 2
-                            }
-                        },
-                        "400": {
-                            "QPSK": {
-                                "max_length": 1298,
-                                "slots_needed": 10
-                            },
-                            "16-QAM": {
-                                "max_length": 581,
-                                "slots_needed": 5
-                            },
-                            "64-QAM": {
-                                "max_length": 219,
-                                "slots_needed": 4
-                            }
-                        }
-                    })
+        return YUE_REACH_ASSUMPTIONS
 
-    return bw_info
+    if sim_type == 'arash':
+        return ARASH_REACH_ASSUMPTIONS
+    raise NotImplementedError(f"Invalid simulation type expected yue or arash and got: {sim_type}")
