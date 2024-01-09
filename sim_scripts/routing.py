c78797f5b36ee84516a4e3766df09a498aea6aad
Ryan McCann <ryan_mccann@student.uml.edu>
2024-01-09 09:57:41 -0500

    Finish rough routing.py

--- sim_scripts/routing.py
+++ sim_scripts/routing.py
@@ -2,10 +2,11 @@ import networkx as nx
 import numpy as np
 
 from arg_scripts.routing_args import empty_props
-from helper_scripts.sim_helpers import find_path_len, get_path_mod
-from helper_scripts.xt_helpers import get_nli_cost
+from helper_scripts.routing_helpers import get_nli_cost, find_xt_link_cost
+from helper_scripts.sim_helpers import find_path_len, get_path_mod, find_free_slots
 
 
+# TODO: Standardize return format
 class Routing:
     """
     This class contains methods related to routing network requests.
@@ -32,14 +33,14 @@ class Routing:
 
         return most_cong_slots
 
-    def _find_least_cong_path(self):
+    def _find_least_cong(self):
         # Sort dictionary by number of free slots, descending
         sorted_paths_list = sorted(self.route_props['paths_list'], key=lambda d: d['link_info']['free_slots'],
                                    reverse=True)
 
         return sorted_paths_list[0]['path']
 
-    def find_least_cong_path(self):
+    def find_least_cong(self):
         """
         Find the least congested path in the network.
 
@@ -49,7 +50,7 @@ class Routing:
         all_paths_obj = nx.shortest_simple_paths(self.sdn_props['topology'], self.route_props['source'],
                                                  self.route_props['destination'])
         min_hops = None
-        least_cong_path = False
+        least_path_list = False
 
         for i, path in enumerate(all_paths_obj):
             num_hops = len(path)
@@ -61,15 +62,15 @@ class Routing:
                     self._find_most_cong_link(path)
                 # We exceeded minimum hops plus one, return the best path
                 else:
-                    least_cong_path = self._find_least_cong_path()
+                    least_path_list = self._find_least_cong()
 
-        self.route_props['paths_list'].append(least_cong_path)
+        self.route_props['paths_list'].append(least_path_list)
         # TODO: Constant QPSK format
         self.route_props['mod_formats_list'].append('QPSK')
         # TODO: Not sure what to put here
         self.route_props['weights_list'].append(None)
 
-    def find_least_weight_path(self, weight: str):
+    def find_least_weight(self, weight: str):
         """
         Find the path with respect to a given weight.
 
@@ -78,40 +79,35 @@ class Routing:
         paths_obj = nx.shortest_simple_paths(G=self.sdn_props['topology'], source=self.route_props['source'],
                                              target=self.route_props['destination'], weight=weight)
 
-        for path in paths_obj:
+        for path_list in paths_obj:
             # If cross-talk, our path weight is the summation across the path
             if weight == 'xt_cost':
-                resp_weight = sum(self.sdn_props['topology'][path[i]][path[i + 1]][weight]
-                                  for i in range(len(path) - 1))
+                resp_weight = sum(self.sdn_props['topology'][path_list[i]][path_list[i + 1]][weight]
+                                  for i in range(len(path_list) - 1))
             else:
-                resp_weight = find_path_len(path=path, topology=self.sdn_props['topology'])
+                resp_weight = find_path_len(path=path_list, topology=self.sdn_props['topology'])
 
             self.route_props['weights_list'].append(resp_weight)
             mod_format = get_path_mod(self.sdn_props['mod_formats'], resp_weight)
             self.route_props['mod_formats_list'].append(mod_format)
-            self.route_props['paths_list'].append(path)
+            self.route_props['paths_list'].append(path_list)
 
-    def k_shortest_path(self, k_paths: int):
-        """
-        Finds the k-shortest-paths.
-
-        :param k_paths: The number of paths to consider.
-        """
+    def find_k_shortest(self):
         # This networkx function will always return the shortest paths in order
         paths_obj = nx.shortest_simple_paths(G=self.sdn_props['topology'], source=self.route_props['source'],
                                              target=self.route_props['destination'], weight='length')
 
-        for k, path in enumerate(paths_obj):
-            if k > k_paths - 1:
+        for k, path_list in enumerate(paths_obj):
+            if k > self.engine_props['k_paths'] - 1:
                 break
-            path_len = find_path_len(path, self.sdn_props['topology'])
+            path_len = find_path_len(path_list, self.sdn_props['topology'])
             mod_format = get_path_mod(self.sdn_props['mod_formats'], path_len)
 
-            self.route_props['paths_list'].append(path)
+            self.route_props['paths_list'].append(path_list)
             self.route_props['mod_formats_list'].append([mod_format])
             self.route_props['weights_list'].append(path_len)
 
-    def nli_aware(self):
+    def find_least_nli(self):
         """
         Finds and selects the path with the least amount of non-linear impairment.
         """
@@ -119,40 +115,64 @@ class Routing:
             source, destination = link[0], link[1]
             num_spans = self.sdn_props['topology'][source][destination]['length'] / self.route_props['span_len']
 
-            link_cost = get_nli_cost(link=link, num_spans=num_spans, source=source,
-                                     destination=destination)
-
+            link_cost = get_nli_cost(link=link, num_spans=num_spans, route_props=self.route_props,
+                                     engine_props=self.engine_props)
             self.sdn_props['topology'][source][destination]['nli_cost'] = link_cost
 
-        self.find_least_weight_path(weight='nli_cost')
+        self.find_least_weight(weight='nli_cost')
 
-    def xt_aware(self, beta: float, xt_type: str):
+    def find_least_xt(self):
         """
-        Calculates all path's costs with respect to intra-core cross-talk values and returns the path with the least
-        amount of cross-talk interference.
+        Finds the path with the least amount of intra-core crosstalk interference.
 
-        :param beta: A parameter used to determine the tradeoff between length and cross-talk value.
-        :param xt_type: Whether we would like to consider length in the final calculation or not.
-        :return: The path with the least amount of interference.
+        :return: The selected path with the least amount of interference.
         :rtype: list
         """
         # At the moment, we have identical bidirectional links (no need to loop over all links)
-        for link in list(self.sdn_props['net_spec_db'].keys())[::2]:
-            source, destination = link[0], link[1]
+        for link_list in list(self.sdn_props['net_spec_db'].keys())[::2]:
+            source, destination = link_list[0], link_list[1]
             num_spans = self.sdn_props['topology'][source][destination]['length'] / self.route_props['span_len']
 
-            free_slots = find_free_slots(net_spec_db=self.sdn_props['net_spec_db'], des_link=link)
-            xt_cost = find_xt_link_cost(free_slots=free_slots, link_num=link)
+            free_slots = find_free_slots(net_spec_db=self.sdn_props['net_spec_db'], des_link=link_list)
+            xt_cost = find_xt_link_cost(free_slots=free_slots, link_num=link_list,
+                                        net_spec_db=self.sdn_props['net_spec_db'])
 
-            if xt_type == 'with_length':
-                link_cost = (beta * (self.sdn_props['topology'][source][destination]['length'] /
-                                     self.route_props['max_link'])) + ((1 - beta) * xt_cost)
-            elif xt_type == 'without_length':
+            if self.engine_props['xt_type'] == 'with_length':
+                link_cost = self.sdn_props['topology'][source][destination]['length'] / self.route_props['max_link']
+                link_cost *= self.engine_props['beta']
+                link_cost += (1 - self.engine_props['beta']) * xt_cost
+            elif self.engine_props['xt_type'] == 'without_length':
                 link_cost = (num_spans / self.route_props['max_span']) * xt_cost
             else:
-                raise ValueError(f'XT type not recognized, expected with or without_length, got: {xt_type}')
+                raise ValueError(f"XT type not recognized, expected with or without_length, "
+                                 f"got: {self.engine_props['xt_type']}")
 
             self.sdn_props['topology'][source][destination]['xt_cost'] = link_cost
             self.sdn_props['topology'][destination][source]['xt_cost'] = link_cost
 
-        self.find_least_weight_path(weight='xt_cost')
+        self.find_least_weight(weight='xt_cost')
+
+    def get_route(self, ai_obj: object):
+        """
+        Controls the class by finding the appropriate routing function.
+
+        :param ai_obj: Artificial intelligence is handled in a separate class.
+        :return: None
+        """
+        if self.engine_props['route_method'] == 'nli_aware':
+            self.find_least_nli()
+        elif self.engine_props['route_method'] == 'xt_aware':
+            self.find_least_xt()
+        elif self.engine_props['route_method'] == 'least_congested':
+            self.find_least_cong()
+        elif self.engine_props['route_method'] == 'shortest_path':
+            self.find_least_weight(weight='length')
+        elif self.engine_props['route_method'] == 'k_shortest_path':
+            self.find_k_shortest()
+        elif self.engine_props['route_method'] == 'ai':
+            # TODO: Need to fix ai to account for passing props, probably don't need all three params
+            path, mod_format = ai_obj.route(sdn_props=self.sdn_props, route_props=self.route_props)
+            self.route_props['paths_list'] = [path]
+            self.route_props['mod_formats_list'] = [mod_format]
+        else:
+            raise NotImplementedError(f"Routing method not recognized, got: {self.engine_props['route_method']}.")
