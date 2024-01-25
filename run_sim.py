caf003313e4d7944cc94ddbca5d9835fe6ac8288
Ryan McCann <ryan_mccann@student.uml.edu>
2024-01-25 09:55:21 -0500

    Update for team standards document

--- run_sim.py
+++ run_sim.py
@@ -29,17 +29,12 @@ class NetworkSimulator:
         # Contains all the desired network simulator parameters for every simulation
         self.properties = None
 
-    def save_input(self, file_name: str, data: dict):
+    def save_input(self, file_name: str, data_dict: dict):
         """
         Saves simulation input data.
 
         :param file_name: The name of the file to save the input data to.
-        :type file_name: str
-
-        :param data: The data to save to the file.
-        :type data: dict
-
-        :return: None
+        :param data_dict: Input data to save.
         """
         path = os.path.join('data', 'input', self.properties['network'], self.properties['date'],
                             self.properties['sim_start'])
@@ -47,115 +42,109 @@ class NetworkSimulator:
         create_dir('data/output')
 
         save_path = os.path.join(path, file_name)
-        with open(save_path, 'w', encoding='utf-8') as file:
-            json.dump(data, file, indent=4)
+        with open(save_path, 'w', encoding='utf-8') as file_path:
+            json.dump(data_dict, file_path, indent=4)
 
-    def create_input(self, local_props: dict):
+    def create_input(self, engine_props: dict):
         """
         Create the input data for the simulation. The method generates bandwidth information, creates the physical
         topology of the network, and creates a dictionary containing all the necessary simulation parameters.
 
-        :return: None
+        :param engine_props: Contains all input parameters for a single simulation/thread.
         """
-        bw_info = create_bw_info(sim_type=local_props['sim_type'])
-        bw_file = f"bw_info_{local_props['thread_num']}.json"
-        self.save_input(file_name=bw_file, data=bw_info)
+        bw_info_dict = create_bw_info(sim_type=engine_props['sim_type'])
+        bw_file = f"bw_info_{engine_props['thread_num']}.json"
+        self.save_input(file_name=bw_file, data_dict=bw_info_dict)
 
-        save_path = os.path.join('data', 'input', local_props['network'], local_props['date'],
-                                 local_props['sim_start'], bw_file)
+        save_path = os.path.join('data', 'input', engine_props['network'], engine_props['date'],
+                                 engine_props['sim_start'], bw_file)
         with open(save_path, 'r', encoding='utf-8') as file_object:
-            local_props['mod_per_bw'] = json.load(file_object)
+            engine_props['mod_per_bw'] = json.load(file_object)
 
-        network_data = create_network(const_weight=local_props['const_link_weight'],
-                                      net_name=local_props['network'])
-        local_props['topology_info'] = create_pt(cores_per_link=local_props['cores_per_link'],
-                                                 network_data=network_data)
+        network_dict = create_network(const_weight=engine_props['const_link_weight'],
+                                      net_name=engine_props['network'])
+        engine_props['topology_info'] = create_pt(cores_per_link=engine_props['cores_per_link'],
+                                                  network_data=network_dict)
 
-        return local_props
+        return engine_props
 
     def _run_yue(self, arr_rate_mean: int, start: int):
         arr_rate_mean = float(arr_rate_mean)
-        local_props = copy.deepcopy(self.properties)
-        local_props['erlang'] = arr_rate_mean / local_props['holding_time']
-        arr_rate_mean *= float(local_props['cores_per_link'])
-        local_props['arrival_rate'] = arr_rate_mean
-        self.create_input(local_props=local_props)
+        engine_props = copy.deepcopy(self.properties)
+        engine_props['erlang'] = arr_rate_mean / engine_props['holding_time']
+        arr_rate_mean *= float(engine_props['cores_per_link'])
+        engine_props['arrival_rate'] = arr_rate_mean
+        self.create_input(engine_props=engine_props)
 
         if arr_rate_mean == start:
             file_name = f"sim_input_{self.properties['thread_num']}.json"
-            self.save_input(file_name=file_name, data=local_props)
+            self.save_input(file_name=file_name, data_dict=engine_props)
 
-        engine = Engine(properties=local_props)
+        engine = Engine(engine_props=engine_props)
         engine.run()
 
     def run_yue(self):
         """
         Runs a simulation using the Yue's simulation assumptions. Reference: Wang, Yue. Dynamic Traffic Scheduling
         Frameworks with Spectral and Spatial Flexibility in Sdm-Eons. Diss. University of Massachusetts Lowell, 2022.
-
-        :return: None
         """
-        arr_rate_obj = self.properties['arrival_rate']
-        start, stop, step = arr_rate_obj['start'], arr_rate_obj['stop'], arr_rate_obj['step']
+        arr_rate_dict = self.properties['arrival_rate']
+        start, stop, step = arr_rate_dict['start'], arr_rate_dict['stop'], arr_rate_dict['step']
 
         if self.properties['thread_erlangs']:
             with concurrent.futures.ProcessPoolExecutor() as executor:
-                futures = []
+                futures_list = []
                 for arr_rate_mean in range(start, stop, step):
                     time.sleep(1.0)
                     future = executor.submit(self._run_yue, arr_rate_mean=arr_rate_mean, start=start)
-                    futures.append(future)
+                    futures_list.append(future)
 
-                for future in concurrent.futures.as_completed(futures):
+                for future in concurrent.futures.as_completed(futures_list):
                     future.result()
         else:
             for arr_rate_mean in range(start, stop, step):
                 self._run_yue(arr_rate_mean=arr_rate_mean, start=start)
 
     def _run_arash(self, erlang: float, first_erlang: float):
-        local_props = copy.deepcopy(self.properties)
-        local_props['arrival_rate'] = (local_props['cores_per_link'] * erlang) / local_props['holding_time']
-        local_props['erlang'] = erlang
-        local_props = self.create_input(local_props=local_props)
+        engine_props = copy.deepcopy(self.properties)
+        engine_props['arrival_rate'] = (engine_props['cores_per_link'] * erlang) / engine_props['holding_time']
+        engine_props['erlang'] = erlang
+        local_props = self.create_input(engine_props=engine_props)
 
         if first_erlang:
-            self.save_input(file_name=f"sim_input_{local_props['thread_num']}.json", data=local_props)
+            self.save_input(file_name=f"sim_input_{local_props['thread_num']}.json", data_dict=local_props)
 
-        engine = Engine(properties=local_props)
+        engine = Engine(engine_props=engine_props)
         engine.run()
 
     def run_arash(self):
         """
         Runs a simulation using the Arash's simulation assumptions.
         Reference: https://doi.org/10.1016/j.comnet.2020.107755.
-
-        :return: None
         """
-        erlang_obj = self.properties['erlangs']
-        start, stop, step = erlang_obj['start'], erlang_obj['stop'], erlang_obj['step']
-        erlang_lst = [float(erlang) for erlang in range(start, stop, step)]
+        erlang_dict = self.properties['erlangs']
+        start, stop, step = erlang_dict['start'], erlang_dict['stop'], erlang_dict['step']
+        erlang_list = [float(erlang) for erlang in range(start, stop, step)]
 
         if self.properties['thread_erlangs']:
             with concurrent.futures.ProcessPoolExecutor() as executor:
-                futures = []
-                for erlang in erlang_lst:
-                    first_erlang = erlang == erlang_lst[0]
+                futures_list = []
+                for erlang in erlang_list:
+                    first_erlang = erlang == erlang_list[0]
                     time.sleep(1.0)
                     future = executor.submit(self._run_arash, erlang=erlang, first_erlang=first_erlang)
-                    futures.append(future)
+                    futures_list.append(future)
 
-                for future in concurrent.futures.as_completed(futures):
+                for future in concurrent.futures.as_completed(futures_list):
                     future.result()
         else:
-            for erlang in erlang_lst:
-                first_erlang = erlang == erlang_lst[0]
+            for erlang in erlang_list:
+                first_erlang = erlang == erlang_list[0]
                 self._run_arash(erlang=erlang, first_erlang=first_erlang)
 
     def run_sim(self, **kwargs):
         """
         Runs all simulations.
-
-        :return: None
         """
         self.properties = kwargs['thread_params']
         # The date and current time derived from the simulation start
@@ -174,20 +163,17 @@ class NetworkSimulator:
             self.run_arash()
 
 
-def run(sims_obj: dict):
+def run(sims_dict: dict):
     """
     Runs multiple simulations concurrently or a single simulation.
 
-    :param sims_obj: Contains the parameters for each simulation.
-    :type sims_obj: dict
-
-    :return: None
+    :param sims_dict: Contains the parameters for each simulation.
     """
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        futures = []
+        futures_list = []
 
         sim_start = datetime.now().strftime("%m%d_%H_%M_%S_%f")
-        for thread_num, thread_params in sims_obj.items():
+        for thread_num, thread_params in sims_dict.items():
             curr_sim = NetworkSimulator()
             class_inst = curr_sim.run_sim
 
@@ -195,13 +181,13 @@ def run(sims_obj: dict):
             future = executor.submit(class_inst, thread_num=thread_num, thread_params=thread_params,
                                      sim_start=sim_start)
 
-            futures.append(future)
+            futures_list.append(future)
 
-        for future in concurrent.futures.as_completed(futures):
+        for future in concurrent.futures.as_completed(futures_list):
             future.result()
 
 
 if __name__ == '__main__':
     args_obj = parse_args()
-    all_sims = read_config(args_obj=args_obj)
-    run(sims_obj=all_sims)
+    all_sims_dict = read_config(args_obj=args_obj)
+    run(sims_dict=all_sims_dict)
