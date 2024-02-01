f8ed17a22ac23bbe62027a81f6b4285281bf5f9f
Ryan McCann <ryan_mccann@student.uml.edu>
2024-02-01 17:17:28 -0500

    Rough draft plot stats

--- plot_scripts/plot_stats.py
+++ plot_scripts/plot_stats.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,6 +9,8 @@ from helper_scripts.os_helpers import create_dir
 from helper_scripts.plot_helpers import PlotHelpers, find_times
 
 
+# TODO: Double check this script and all scripts tomorrow
+# TODO: Add number of cores and number of requests
 class PlotStats:
     """
     A class for computing and plotting statistical analysis for simulations.
@@ -15,19 +18,25 @@ class PlotStats:
 
     def __init__(self, sims_info_dict: dict):
         self.plot_props = empty_props
+        self.sims_info_dict = sims_info_dict
         self.plot_help_obj = PlotHelpers(plot_props=self.plot_props, net_names_list=sims_info_dict['networks_matrix'])
 
         self.plot_help_obj.get_file_info(sims_info_dict=sims_info_dict)
 
     def _save_plot(self, file_name: str):
-        # file_path = f"./output/{self.net_names[0][-1]}/{self.dates[0][-1]}/{self.plot_props['time']s[0][-1]}"
-        # create_dir(file_path)
-        # plt.savefig(f'{file_path}/{file_name}_core{self.num_cores}.png')
-        pass
+        # Default to the earliest time for saving
+        time = self.sims_info_dict['times_matrix'][0][-1]
+        network = self.sims_info_dict['networks_matrix'][0][-1]
+        date = self.sims_info_dict['dates_matrix'][0][-1]
+        save_fp = os.path.join('..', 'data', 'plots', network, date, time)
+        create_dir(file_path=save_fp)
+
+        save_fp = os.path.join(save_fp, file_name)
+        plt.savefig(save_fp)
 
     def _setup_plot(self, title, y_label, x_label, grid=True, y_ticks=True, y_lim=False, x_ticks=True):
         plt.figure(figsize=(7, 5), dpi=300)
-        plt.title(f"{self.plot_props['title_names']} {title} R={self.plot_props['num_requests']}")
+        plt.title(f"{self.plot_props['title_names']} {title}")
         plt.ylabel(y_label)
         plt.xlabel(x_label)
 
@@ -92,17 +101,20 @@ class PlotStats:
             self._save_plot(file_name=f"{file_name}_{alpha}_{gamma}_{erlangs[index]}")
             plt.show()
 
-    # TODO: Add a proper legend
     def _plot_helper_one(self, x_vals: str, y_vals: str, file_name: str):
         legend_list = list()
         color_count = 0
+        style_count = 0
         for _, sims_dict in self.plot_props['plot_dict'].items():
             for _, info_dict in sims_dict.items():
                 color = self.plot_props['color_list'][color_count]
-                style = 'solid'
+                style = self.plot_props['style_list'][style_count]
                 plt.plot(info_dict[x_vals], info_dict[y_vals], linestyle=style, markersize=2.3, color=color)
                 legend_list.append(color)
                 color_count += 1
+
+            color_count = 0
+            style_count += 1
 
         plt.legend(legend_list)
         self._save_plot(file_name=file_name)
@@ -164,7 +176,7 @@ class PlotStats:
         Plots the average blocking probability for each Erlang value.
         """
         self._setup_plot("Average Blocking Prob. vs. Erlang", 'Average Blocking Probability', 'Erlang', y_ticks=True)
-        self._plot_helper_one(x_vals='erlang_list', y_vals='blocking_list', file_name='average_bp')
+        self._plot_helper_one(x_vals='erlang_list', y_vals='blocking_list', file_name='average_bp.png')
 
 
 def main():
@@ -184,8 +196,6 @@ def main():
     sims_info_dict = find_times(dates_dict={'0201': 'USNet'}, filter_dict=filter_dict)
     plot_obj = PlotStats(sims_info_dict=sims_info_dict)
 
-    # TODO: Transfer to a loop or something creative
-    # TODO: Double check that plot dict is actually correct
     plot_obj.plot_blocking()
     # plot_obj.plot_path_length()
     # plot_obj.plot_hops()
