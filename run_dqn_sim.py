765044a95815f7dbf22236f06b37ad6296060360
Ryan McCann <ryan_mccann@student.uml.edu>
2024-03-05 15:01:36 -0500

    Safety commit computer freezing

--- run_dqn_sim.py
+++ run_dqn_sim.py
@@ -1,9 +1,11 @@
+import copy
 import os
 from datetime import datetime
 
 import gymnasium as gym
 import numpy as np
 from gymnasium import spaces
+from stable_baselines3 import DQN
 
 from config_scripts.parse_args import parse_args
 from config_scripts.setup_config import read_config
@@ -33,28 +35,29 @@ class DQNSimEnv(gym.Env):
         self.engine_obj = None
         self.route_obj = None
         self.mock_sdn = dict()
-        self.setup()
 
-        self.engine_props = self.engine_obj.engine_props
-        self.observation_space = spaces.Box(low=0.0, high=1.0,
-                                            shape=(self.engine_props['cores_per_link'],
-                                                   self.engine_props['spectral_slots']),
-                                            dtype=np.float64)
-        self.action_space = spaces.Discrete(self.engine_props['cores_per_link'] *
-                                            self.engine_props['spectral_slots'])
+        self.engine_props = None
+        # TODO: Static for now
+        self.observation_space = spaces.Box(low=0.0, high=1.0, shape=(1, 128), dtype=np.float64)
+        self.action_space = spaces.Discrete(1 * 128)
 
+        self.arrival_reqs_dict = dict()
+        self.depart_reqs_dict = dict()
         self.iteration = 0
-        self.req_num = 1
+        self.arrival_count = 0
+
         self.curr_time = None
 
     def _check_terminated(self):
-        if self.req_num == ((self.engine_props['num_requests'] * 2) + 1):
+        if self.arrival_count == (self.engine_props['num_requests']):
             terminated = True
-            self.engine_obj.end_iter(iteration=self.iteration, print_flag=False, ai_flag=True)
+            base_fp = os.path.join('..', 'data')
+            self.engine_obj.end_iter(iteration=self.iteration, print_flag=False, ai_flag=True, base_fp=base_fp)
             self.iteration += 1
         else:
             terminated = False
 
+        print(f'Did we terminate? --> {terminated}')
         return terminated
 
     @staticmethod
@@ -67,7 +70,7 @@ class DQNSimEnv(gym.Env):
         return reward
 
     def _update_reqs_status(self, was_routed: bool, mod_format: str = None, path_list: list = None):
-        self.engine_obj.reqs_status_dict.update({self.engine_obj.reqs_dict[self.curr_time]['req_id']: {
+        self.engine_obj.reqs_status_dict.update({self.arrival_reqs_dict[self.curr_time]['req_id']: {
             "mod_format": mod_format,
             "path": path_list,
             "is_sliced": False,
@@ -78,11 +81,12 @@ class DQNSimEnv(gym.Env):
         for link_tuple in zip(path_list, path_list[1:]):
             rev_link_tuple = (link_tuple[1], link_tuple[0])
 
-            self.engine_obj.net_spec_dict[link_tuple]['cores_matrix'][core_num][start_slot:end_slot] = self.req_num
-            self.engine_obj.net_spec_dict[rev_link_tuple]['cores_matrix'][core_num][start_slot:end_slot] = self.req_num
+            req_id = self.arrival_reqs_dict[self.arrival_count]['request_id']
+            self.engine_obj.net_spec_dict[link_tuple]['cores_matrix'][core_num][start_slot:end_slot] = req_id
+            self.engine_obj.net_spec_dict[rev_link_tuple]['cores_matrix'][core_num][start_slot:end_slot] = req_id
 
-            self.engine_obj.net_spec_dict[link_tuple]['cores_matrix'][core_num][end_slot] = self.req_num * -1
-            self.engine_obj.net_spec_dict[rev_link_tuple]['cores_matrix'][core_num][end_slot] = self.req_num * -1
+            self.engine_obj.net_spec_dict[link_tuple]['cores_matrix'][core_num][end_slot] = req_id * -1
+            self.engine_obj.net_spec_dict[rev_link_tuple]['cores_matrix'][core_num][end_slot] = req_id * -1
 
     def _check_is_free(self, path_list: list, core_num: int, start_slot: int, end_slot: int):
         is_free = True
@@ -109,8 +113,9 @@ class DQNSimEnv(gym.Env):
             self.engine_obj.net_spec_dict[source_dest]['cores_matrix'][core_num][gb_index] = 0
             self.engine_obj.net_spec_dict[dest_source]['cores_matrix'][core_num][gb_index] = 0
 
-    def release(self):
-        arrival_id = self.engine_obj.reqs_dict[self.curr_time]['req_id']
+    # TODO: Check this
+    def release(self, depart_time: float):
+        arrival_id = self.engine_obj.reqs_dict[depart_time]['req_id']
         if self.engine_obj.reqs_status_dict[arrival_id]['was_routed']:
             path_list = self.engine_obj.reqs_status_dict[arrival_id]['path']
 
@@ -128,6 +133,19 @@ class DQNSimEnv(gym.Env):
         # Request was blocked
         else:
             pass
+
+    def check_release(self):
+        curr_time = self.arrival_reqs_dict[self.arrival_count]
+        depart_times_list = list()
+        for depart_time in sorted(self.depart_reqs_dict):
+            if depart_time <= curr_time:
+                self.release(depart_time=depart_time)
+                depart_times_list.append(depart_time)
+            else:
+                break
+
+        for depart_time in depart_times_list:
+            self.depart_reqs_dict.pop(depart_time)
 
     def _allocate(self, is_free: bool, path_list: list, start_slot: int, end_slot: int, core_num: int, mod_format: str,
                   bandwidth: str, path_len: float):
@@ -166,7 +184,8 @@ class DQNSimEnv(gym.Env):
                 was_allocated = False
                 continue
 
-            bandwidth = self.engine_obj.reqs_dict[self.curr_time]['bandwidth']
+            # TODO: Change
+            bandwidth = self.arrival_reqs_dict[self.arrival_count]['bandwidth']
             bandwidth_dict = self.engine_props['mod_per_bw'][bandwidth]
             end_slot = start_slot + bandwidth_dict[mod_format]['slots_needed']
             if end_slot >= self.engine_props['spectral_slots']:
@@ -187,27 +206,17 @@ class DQNSimEnv(gym.Env):
 
     # fixme: Failure if second request is a departure
     def step(self, action: int):
-        print(f'== stepped with request number {self.req_num} ===')
-
         core_num = action // self.engine_props['spectral_slots']
         start_slot = action % self.engine_props['spectral_slots']
+
+        self.check_release()
 
         was_allocated = self.allocate(core_num=core_num, start_slot=start_slot)
         self.engine_obj.update_arrival_params(curr_time=self.curr_time, ai_flag=True, mock_sdn=self.mock_sdn)
         reward = self._calculate_reward(was_allocated=was_allocated)
 
-        while True:
-            self.curr_time = list(self.engine_obj.reqs_dict.keys())[self.req_num - 1]
-            if self.engine_obj.reqs_dict[self.curr_time]['request_type'] == 'release':
-                self.release()
-                self.req_num += 1
-                terminated = self._check_terminated()
-                if terminated:
-                    break
-            else:
-                self.req_num += 1
-                terminated = self._check_terminated()
-                break
+        self.arrival_count += 1
+        terminated = self._check_terminated()
 
         new_obs = self._get_obs()
         truncated = False
@@ -271,7 +280,8 @@ class DQNSimEnv(gym.Env):
         return dict()
 
     def _get_obs(self):
-        curr_req = self.engine_obj.reqs_dict[self.curr_time]
+        # TODO: Change
+        curr_req = self.arrival_reqs_dict[self.arrival_count]
         self._update_mock_sdn(curr_req=curr_req)
 
         self.route_obj.sdn_props = self.mock_sdn
@@ -282,17 +292,22 @@ class DQNSimEnv(gym.Env):
         return spectrum_obs
 
     def reset(self, seed: int = None, options: dict = None):
-        print('=== made it to reset ===')
         super().reset(seed=seed)
+        self.setup()
+        self.engine_props = self.engine_obj.engine_props
 
-        self.req_num = 1
+        self.arrival_count = 0
         self.engine_obj.init_iter(iteration=self.iteration)
         self.engine_obj.create_topology()
 
         if seed is None:
             seed = self.iteration
         self.engine_obj.generate_requests(seed=seed)
-        self.curr_time = list(self.engine_obj.reqs_dict.keys())[self.req_num - 1]
+        for req_time in self.engine_obj.reqs_dict:
+            if self.engine_obj.reqs_dict[req_time]['request_type'] == 'arrival':
+                self.arrival_reqs_dict[req_time] = copy.deepcopy(self.engine_obj.reqs_dict[req_time])
+            else:
+                self.depart_reqs_dict[req_time] = copy.deepcopy(self.engine_obj.reqs_dict[req_time])
 
         obs = self._get_obs()
         info = self._get_info()
@@ -308,13 +323,13 @@ class DQNSimEnv(gym.Env):
 
     # TODO: Have number of time steps be the number of reqs
     def setup(self):
-        print('=== made it to setup. ===')
-
         args_obj = parse_args()
-        config_path = os.path.join('ini', 'run_ini', 'config.ini')
+        # TODO: Here
+        config_path = os.path.join('..', 'ini', 'run_ini', 'config.ini')
         self.dqn_sim_dict = read_config(args_obj=args_obj, config_path=config_path)
 
-        base_fp = os.path.join('data')
+        # TODO: Here
+        base_fp = os.path.join('..', 'data')
         self.dqn_sim_dict['s1']['thread_num'] = 's1'
         self._get_start_time()
         self.dqn_sim_dict['s1'] = create_input(base_fp=base_fp, engine_props=self.dqn_sim_dict['s1'])
@@ -328,3 +343,19 @@ class DQNSimEnv(gym.Env):
         start_arr_rate = float(self.dqn_sim_dict['s1']['arrival_rate']['start'])
         self.engine_obj.engine_props['erlang'] = start_arr_rate / self.dqn_sim_dict['s1']['holding_time']
         self.engine_obj.engine_props['arrival_rate'] = start_arr_rate * self.dqn_sim_dict['s1']['cores_per_link']
+
+
+if __name__ == '__main__':
+    env = DQNSimEnv()
+
+    model = DQN("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=20, log_interval=5)
+
+    # obs, info = env.reset()
+    # while True:
+    #     curr_action, _states = model.predict(obs, deterministic=True)
+    #
+    #     obs, curr_reward, is_terminated, is_truncated, curr_info = env.step(curr_action)
+    #     if is_terminated or is_truncated:
+    #         break
+    # obs, info = env.reset()
