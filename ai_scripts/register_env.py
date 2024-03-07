dd5e99d17dee658dd245a6cfb51074180731596d
Ryan McCann <ryan_mccann@student.uml.edu>
2024-03-07 12:04:01 -0500

    Lint register_env.py

--- ai_scripts/register_env.py
+++ ai_scripts/register_env.py
@@ -6,12 +6,20 @@ from gymnasium.envs.registration import register
 
 
 def copy_yml_file(algorithm: str):
+    """
+    Copies a yaml file to the RLZoo3 library.
+
+    :param algorithm: The algorithm being used for RLZoo3.
+    """
     source_file = f'ai_scripts/yml/{algorithm}.yml'
     destination_file = f'venvs/unity_venv/venv/lib/python3.11/site-packages/rl_zoo3/hyperparams/{algorithm}.yml'
     shutil.copy(source_file, destination_file)
 
 
 def main():
+    """
+    Controls this script.
+    """
     parser = argparse.ArgumentParser(description='Register your custom Gymnasium environment.')
     parser.add_argument('--algo', help='The custom algorithm yml file.')
     parser.add_argument('--env-name', help='The environment to register')
