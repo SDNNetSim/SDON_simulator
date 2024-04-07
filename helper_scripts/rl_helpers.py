1afea1175085233538025505f7432a2992c5886b
Ryan McCann <ryan_mccann@student.uml.edu>
2024-04-07 14:29:54 -0400

    Safety commit

--- helper_scripts/rl_helpers.py
+++ helper_scripts/rl_helpers.py
@@ -41,12 +41,20 @@ class RLHelpers:
                                             spectral_slots=self.ai_props['spectral_slots'], core_num=self.core_num,
                                             slots_needed=slots_needed)
 
-        # TODO: What to do if there are less than 4 super channels or link is fully congested (all inf)
+        # TODO: What to do if there are less than 4 (or requested) super channels or link is fully congested (all inf)
         self.super_channel_indexes = sc_index_mat[0:num_channels]
         resp_frag_mat = list()
         for channel in self.super_channel_indexes:
             start_index = channel[0]
             resp_frag_mat.append(hfrag_arr[start_index])
+
+        # TODO: Hard coded 4
+        # TODO: Don't forget to scale!
+        if len(resp_frag_mat) < 4 or np.any(np.isinf(resp_frag_mat)):
+            resp_frag_mat = np.where(np.isinf(resp_frag_mat), 1.0, resp_frag_mat)
+            difference = 4 - len(resp_frag_mat)
+            for i in range(difference):
+                resp_frag_mat[4 - i] = 1.0
 
         return resp_frag_mat
 
@@ -224,10 +232,7 @@ class RLHelpers:
         """
         path_matrix = [route_obj.route_props['paths_list'][self.path_index]]
         curr_time = self.ai_props['arrival_list'][self.ai_props['arrival_count']]['arrive']
-        try:
-            start_index = self.super_channel_indexes[self.super_channel][0]
-        except:
-            print('Here')
+        start_index = self.super_channel_indexes[self.super_channel][0]
         self.engine_obj.handle_arrival(curr_time=curr_time, force_route_matrix=path_matrix,
                                        forced_index=start_index)
 
