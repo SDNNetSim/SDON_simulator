3bd2e5a806d4976a110e80f9ca5abd1f2dff412c
Ryan McCann <ryan_mccann@student.uml.edu>
2024-04-16 15:17:52 -0400

    Finish statistics_helpers docs
    Also lint stats_helpers.py

--- docs/helper/stats_helpers.rst
+++ docs/helper/stats_helpers.rst
@@ -1,6 +1,11 @@
 Statistics Helpers
-==================
+===================
+
+The Statistics Helpers encompass functions designed to assist with statistical analysis and data processing tasks
+within the simulator. These utilities facilitate the collection, organization, and analysis of simulation data,
+enabling users to derive valuable insights and draw meaningful conclusions from simulation results.
 
 .. automodule:: helper_scripts.stats_helpers
     :members:
     :undoc-members:
+    :private-members:
--- helper_scripts/stats_helpers.py
+++ helper_scripts/stats_helpers.py
@@ -187,11 +187,8 @@ class SimStats:
             self.stats_props['route_times_list'].append(sdn_data['route_time'])
             self.total_trans += sdn_data['num_trans']
             bandwidth = sdn_data['bandwidth']
+            mod_format = sdn_data['modulation_list'][0]
 
-            try:
-                mod_format = sdn_data['modulation_list'][0]
-            except:
-                print('line 194 stats helpers')
             self.stats_props['weights_dict'][bandwidth][mod_format].append(sdn_data['path_weight'])
 
     def _get_iter_means(self):
@@ -212,7 +209,6 @@ class SimStats:
                     if len(data_list) == 1:
                         deviation = 0.0
                     else:
-                        # TODO: Weights dict not updates properly (empty lists)
                         deviation = stdev(data_list)
                     mod_obj[modulation] = {'mean': mean(data_list), 'std': deviation,
                                            'min': min(data_list), 'max': max(data_list)}
