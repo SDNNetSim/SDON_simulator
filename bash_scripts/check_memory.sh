4a69a57b13556cad58d9b28792bf285c5097315b
Ryan McCann <ryan_mccann@student.uml.edu>
2024-04-16 11:42:59 -0400

    generalize check_memory.sh
    Process name is now a variable

--- bash_scripts/check_memory.sh
+++ bash_scripts/check_memory.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-	echo "Usage: $0 <script_path>"
-	exit 1
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <script_path> <process_name>"
+  exit 1
 fi
 
 script_path="$1"
 script_name=$(basename "$script_path")
+process_name="$2"
 
 if [ ! -f "$script_path" ]; then
-	echo "Error: $script_path is not a valid file path."
-	exit 1
+  echo "Error: $script_path is not a valid file path."
+  exit 1
 fi
 
-ps u -p $(pgrep -d',' -f run_sim.py) | awk '{sum += $6} END {print "Total memory used is: " sum / 1024 " MB" " or " sum / 1024 / 1024 " GB"}'
+ps u -p $(pgrep -d',' -f "$process_name") | awk '{sum += $6} END {print "Total memory used is: " sum / 1024 " MB" " or " sum / 1024 / 1024 " GB"}'
 
 num_proc=$(pgrep -fc "$script_name")
-echo "Total number of processes used by $script_name is: $num_proc" 
+echo "Total number of processes used by $script_name is: $num_proc"
