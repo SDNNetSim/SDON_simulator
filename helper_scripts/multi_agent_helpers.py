9815101f85654c225e50f6350f22f76f694d97b6
Ryan McCann <ryan_mccann@student.uml.edu>
2024-04-24 15:14:47 -0400

    Add multi_agent_helpers script
    Multi-agent will be a script where the user can highly customize how RL is used in simulations.
    
    Also, add an example image to the getting started docs.

---
+++ helper_scripts/multi_agent_helpers.py
@@ -0,0 +1,13 @@
+class PathAgent:
+    def __init__(self):
+        raise NotImplementedError
+
+
+class CoreAgent:
+    def __init__(self):
+        raise NotImplementedError
+
+
+class SpectrumAgent:
+    def __init__(self):
+        raise NotImplementedError
