29660f5a03685c5a5b47627803360b7d2c53e42e
Ryan McCann <ryan_mccann@student.uml.edu>
2024-05-15 22:06:05 +0000

    Update run sim

--- bash_scripts/run_sim.sh
+++ bash_scripts/run_sim.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-#SBATCH -p cpu-preempt
+#SBATCH -p cpu-long
 #SBATCH -c 1
 #SBATCH -G 0
-#SBATCH --mem=50000
-#SBATCH -t 2-00:00:00
+#SBATCH --mem=16000
+#SBATCH -t 4-00:00:00
 #SBATCH -o slurm-%j.out
 
 # shellcheck disable=SC2164
-# cd
+cd
 # shellcheck disable=SC2164
-# cd /work/pi_vinod_vokkarane_uml_edu/git/sdn_simulator/
+cd /work/pi_vinod_vokkarane_uml_edu/git/sdn_simulator/
 
 # Make and activate virtual environment
-#rm -rf venvs/unity_venv/venv
-#module load python/3.11.0
-#./bash_scripts/make_venv.sh venvs/unity_venv python3.11
-# source venvs/unity_venv/venv/bin/activate
+rm -rf venvs/unity_venv/venv
+module load python/3.11.0
+./bash_scripts/make_venv.sh venvs/unity_venv python3.11
+source venvs/unity_venv/venv/bin/activate
 
 # Download requirements
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Modify StableBaselines3 to register custom environments
-#./bash_scripts/register_rl_env.sh ppo SimEnv
+./bash_scripts/register_rl_env.sh ppo SimEnv
 
 # Run AI simulation
 # python -m rl_zoo3.train --algo ppo --env SimEnv --conf-file ./ai_scripts/yml/ppo.yml -optimize --n-trials 5 --n-timesteps 20000
@@ -35,7 +35,7 @@ var1_list=("0.1" "0.1" "0.2" "0.2" "0.8" "0.8" "0.9" "0.9")
 var2_list=("0.99" "0.1" "0.99" "0.1" "0.99" "0.1" "0.99" "0.1")
 var3_list=("0.10" "0.10" "0.10" "0.10" "0.10" "0.10" "0.10" "0.10")
 
-python run_rl_sim.py --learn_rate "${var1_list[${SLURM_ARRAY_TASK_ID}]}" --discount_factor "${var2_list[${SLURM_ARRAY_TASK_ID}]}" --epsilon_start "${var3_list[${SLURM_ARRAY_TASK_ID}]}"
+python run_rl_sim.py --device gpu --path_algorithm first_fit --core_algorithm q_learning --spectrum_algorithm first_fit
 
 # Run regular simulation
 # python run_sim.py --max_segments 1 --k_paths 3
