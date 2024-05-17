6b846e6313d9129592dd8b2ab7925d34616fa33a
Ryan McCann <ryan_mccann@student.uml.edu>
2024-05-17 14:10:33 +0000

    Update run_sim.sh to include rewards and penalties

--- bash_scripts/run_sim.sh
+++ bash_scripts/run_sim.sh
@@ -4,7 +4,7 @@
 #SBATCH -c 1
 #SBATCH -G 0
 #SBATCH --mem=16000
-#SBATCH -t 4-00:00:00
+#SBATCH -t 2-00:00:00
 #SBATCH -o slurm-%j.out
 
 # shellcheck disable=SC2164
@@ -13,25 +13,35 @@ cd
 cd /work/pi_vinod_vokkarane_uml_edu/git/sdn_simulator/
 
 # Make and activate virtual environment
-rm -rf venvs/unity_venv/venv
+# rm -rf venvs/unity_venv/venv
 module load python/3.11.0
-./bash_scripts/make_venv.sh venvs/unity_venv python3.11
+# ./bash_scripts/make_venv.sh venvs/unity_venv python3.11
 source venvs/unity_venv/venv/bin/activate
 
 # Download requirements
 pip install -r requirements.txt
 
 # Modify StableBaselines3 to register custom environments
-./bash_scripts/register_rl_env.sh ppo SimEnv
+# ./bash_scripts/register_rl_env.sh ppo SimEnv
 
 # Learning rate, discount factor, epsilon
 
 # Variables for outer loop
-var1_list=("0.1" "0.1" "0.2" "0.2" "0.8" "0.8" "0.9" "0.9")
-var2_list=("0.99" "0.1" "0.99" "0.1" "0.99" "0.1" "0.99" "0.1")
-var3_list=("0.10" "0.10" "0.10" "0.10" "0.10" "0.10" "0.10" "0.10")
+learn_rate_list=("0.01" "0.01" "0.9" "0.9" "0.01" "0.01" "0.9" "0.9" "0.01" "0.01" "0.9" "0.9")
+disc_factor_list=("0.9" "0.1" "0.9" "0.1" "0.9" "0.1" "0.9" "0.1" "0.9" "0.1" "0.9" "0.1")
+epsilon_start_list=("0.30" "0.30" "0.30" "0.30" "0.30" "0.30" "0.30" "0.30" "0.30" "0.30" "0.30" "0.30")
+reward_list=("1.0" "1.0" "1.0" "1.0" "1.0" "1.0" "1.0" "1.0" "1.0" "1.0" "1.0" "1.0")
+penalty_list=("-1.0" "-1.0" "-1.0" "-1.0" "0.0" "0.0" "0.0" "0.0" "-10.0" "-10.0" "-10.0" "-10.0")
 
-python run_rl_sim.py --device cpu --path_algorithm first_fit --core_algorithm q_learning --spectrum_algorithm first_fit
+INDEX=$SLURM_ARRAY_TASK_ID
+# Extract values based on index
+LEARN_RATE=${learn_rate_list[$INDEX]}
+DISC_FACTOR=${disc_factor_list[$INDEX]}
+EPSILON_START=${epsilon_start_list[$INDEX]}
+REWARD=${reward_list[$INDEX]}
+PENALTY=${penalty_list[$INDEX]}
+
+python run_rl_sim.py --path_algorithm q_learning --core_algorithm first_fit --spectrum_algorithm first_fit --learn_rate $LEARN_RATE --discount_factor $DISC_FACTOR --epsilon_start $EPSILON_START --reward $REWARD --penalty $PENALTY
 
 # Run regular simulation
 # python run_sim.py --max_segments 1 --k_paths 3
