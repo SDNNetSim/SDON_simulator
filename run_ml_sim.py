0d3b4018f07a1171fe0d7b7685ad8222f713793d
Ryan McCann <ryan_mccann@student.uml.edu>
2024-05-22 13:29:36 -0400

    Structured the run_ml_sim.py file for ML simulation

--- run_ml_sim.py
+++ run_ml_sim.py
@@ -1,4 +1,49 @@
-# TODO: Any overlap with run_rl_sim should be added to sim helpers or something similar
+# Import necessary libraries and modules
 
-if __name__ == '__main__'
-    raise NotImplementedError
+
+# Define the custom environment for the ML simulation
+class SimEnv:
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+
+
+# Define helper functions
+def _run_iters():
+    pass
+
+
+def _get_model():
+    pass
+
+
+def _print_info():
+    pass
+
+
+def _get_trained_model():
+    pass
+
+
+def _run_ml_sim(env: object):
+    pass
+
+
+def _setup_ml_sim():
+    pass
+
+
+# Define the main function that controls the ML simulations
+def run_ml_sim():
+    env = SimEnv()
+    _run_ml_sim(env=env)
+
+
+# If the script is run directly, start the simulation
+if __name__ == '__main__':
+    run_ml_sim()
