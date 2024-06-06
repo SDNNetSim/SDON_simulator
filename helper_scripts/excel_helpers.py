b14215e9ebd1777bf29b90919a7313f1ae47cb56
Ryan McCann <ryan_mccann@student.uml.edu>
2024-06-06 16:43:36 -0400

    Make the read files function

--- helper_scripts/excel_helpers.py
+++ helper_scripts/excel_helpers.py
@@ -1,10 +1,10 @@
+# TODO: This might break into its own module
 import os
 import json
 
 from helper_scripts.plot_helpers import find_times, PlotHelpers
 from arg_scripts.plot_args import empty_props
 
-# TODO: This might break into its own module
 filter_dict = {
     'and_filter_list': [
     ],
@@ -20,7 +20,20 @@ sims_info_dict = find_times(dates_dict={'0606': 'NSFNet'}, filter_dict=filter_di
 helpers_obj = PlotHelpers(plot_props=empty_props, net_names_list=sims_info_dict['networks_matrix'])
 helpers_obj.get_file_info(sims_info_dict=sims_info_dict)
 
-# TODO: We will only have 's1' for the time being
+
+def read_files():
+    output_fp = os.path.join('..', 'data', 'output', network, date, run_time, 's1',
+                             f'{erlang}_erlang.json')
+    input_fp = os.path.join('..', 'data', 'input', network, date, run_time, f'sim_input_s1.json')
+    with open(output_fp, 'r') as file_path:
+        output_dict = json.load(file_path)
+    with open(input_fp, 'r') as file_path:
+        input_dict = json.load(file_path)
+
+    return input_dict, output_dict
+
+
+# TODO: We will only have 's1' for now
 for run_time, run_obj in helpers_obj.file_info.items():
     net_key, date_key, sim_key = list(run_obj.keys())
     network = run_obj[net_key]
@@ -28,7 +41,5 @@ for run_time, run_obj in helpers_obj.file_info.items():
     sim_dict = run_obj[sim_key]
     erlang = sim_dict['s1'][0]
 
-    file_path = os.path.join('..', 'data', 'output', network, date, run_time, 's1',
-                             f'{erlang}_erlang.json')
-    with open(file_path, 'r') as file_obj:
-        file_dict = json.load(file_obj)
+    input_dict, output_dict = read_files()
+    print('Here')
