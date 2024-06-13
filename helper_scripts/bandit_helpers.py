40cc3c94521d586a77cd622db60913fc36073801
Ryan McCann <ryan_mccann@student.uml.edu>
2024-06-13 16:12:42 -0400

    Add a context generator
    Also some important TODOs

--- helper_scripts/bandit_helpers.py
+++ helper_scripts/bandit_helpers.py
@@ -1,8 +1,14 @@
 import numpy as np
 
+from sklearn.linear_model import LinearRegression
+
 from arg_scripts.rl_args import empty_bandit_props
 
 
+# TODO: Save model and other results (Use this as a baseline?)
+#   - For a contextual bandit, similar to the q-table:
+#       - Source, destination, each of the k-paths
+#       - Later on, congestion levels?
 class MultiBanditHelpers:
     def __init__(self, rl_props: dict, engine_props: dict):
         self.props = empty_bandit_props
@@ -35,3 +41,50 @@ class MultiBanditHelpers:
 
     def setup_env(self):
         pass
+
+
+class ContextualEpsilonGreedyHelpers:
+    def __init__(self, n_arms: int, n_features: int, epsilon: float):
+        self.n_arms = n_arms
+        self.epsilon = epsilon
+        self.models = [LinearRegression() for _ in range(n_arms)]
+        self.X = [np.empty((0, n_features)) for _ in range(n_arms)]
+        self.y = [np.empty((0,)) for _ in range(n_arms)]
+        self.is_fitted = [False] * n_arms
+
+    def select_arm(self, context):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(self.n_arms)
+        else:
+            estimated_rewards = []
+            for i, model in enumerate(self.models):
+                if self.is_fitted[i]:
+                    estimated_reward = model.predict(context.reshape(1, -1))[0]
+                else:
+                    estimated_reward = 0
+                estimated_rewards.append(estimated_reward)
+            return np.argmax(estimated_rewards)
+
+    def update(self, arm, context, reward):
+        self.X[arm] = np.vstack([self.X[arm], context])
+        self.y[arm] = np.append(self.y[arm], reward)
+
+        self.models[arm].fit(self.X[arm], self.y[arm])
+        self.is_fitted[arm] = True
+
+
+class ContextGenerator:
+    def __init__(self, num_sources: int, num_destinations: int, num_paths: int):
+        self.num_sources = num_sources
+        self.num_destinations = num_destinations
+        self.num_paths = num_paths
+
+    def generate_context(self, source, destination, congestion_levels):
+        source_vec = np.zeros(self.num_sources)
+        source_vec[source] = 1
+
+        dest_vec = np.zeros(self.num_destinations)
+        dest_vec[destination] = 1
+
+        context = np.concatenate([source_vec, dest_vec, congestion_levels])
+        return context
