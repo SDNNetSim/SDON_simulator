39d9d592a4a371a8aef94e41f553841673aca441
Ryan McCann <ryan_mccann@student.uml.edu>
2024-07-02 16:11:48 -0400

    Add a new props object to plot_stats.py
    Related to the new plot_args script converting dictionaries to classes

--- plot_scripts/plot_stats.py
+++ plot_scripts/plot_stats.py
@@ -2,7 +2,7 @@ import os
 
 import matplotlib.pyplot as plt
 
-from arg_scripts.plot_args import empty_props
+from arg_scripts.plot_args import EmptyPlotProps
 from helper_scripts.os_helpers import create_dir
 from helper_scripts.plot_helpers import PlotHelpers, find_times
 
@@ -13,9 +13,9 @@ class PlotStats:
     """
 
     def __init__(self, sims_info_dict: dict):
-        self.plot_props = empty_props
+        self.props_obj = EmptyPlotProps()
         self.sims_info_dict = sims_info_dict
-        self.plot_help_obj = PlotHelpers(plot_props=self.plot_props, net_names_list=sims_info_dict['networks_matrix'])
+        self.plot_help_obj = PlotHelpers(plot_props=self.props_obj, net_names_list=sims_info_dict['networks_matrix'])
 
         self.plot_help_obj.get_file_info(sims_info_dict=sims_info_dict)
 
@@ -33,7 +33,7 @@ class PlotStats:
     def _setup_plot(self, title: str, y_lim: list, y_label: str, x_label: str, grid: bool = True, y_ticks: bool = True,
                     x_ticks: bool = True):
         plt.figure(figsize=(7, 5), dpi=300)
-        plt.title(f"{self.plot_props['title_names']} {title}")
+        plt.title(f"{self.props_obj.title_names} {title}")
         plt.ylabel(y_label)
         plt.xlabel(x_label)
 
@@ -46,8 +46,8 @@ class PlotStats:
             plt.ylim(y_lim[0], y_lim[1])
 
         if x_ticks:
-            plt.xticks(self.plot_props['x_tick_list'])
-            plt.xlim(self.plot_props['x_tick_list'][0], self.plot_props['x_tick_list'][-1])
+            plt.xticks(self.props_obj.x_tick_list)
+            plt.xlim(self.props_obj.x_tick_list[0], self.props_obj.x_tick_list[-1])
 
         if grid:
             plt.grid()
@@ -58,20 +58,24 @@ class PlotStats:
         """
         color_count = 0
         style_count = 0
-        for _, sims_dict in self.plot_props['plot_dict'].items():
+        for _, sims_dict in self.props_obj.plot_dict.items():
             for _, info_dict in sims_dict.items():
-                style = self.plot_props['style_list'][style_count]
+                style = self.props_obj.style_list[style_count]
 
                 for y_val in y_vals_list:
-                    color = self.plot_props['color_list'][color_count]
+                    color = self.props_obj.color_list[color_count]
                     index = [index for index, value in enumerate(info_dict['erlang_list']) if value == erlang][0]
                     x_vals = list(range(len(info_dict[y_val][index])))
                     plt.plot(x_vals, info_dict[y_val][index], linestyle=style, markersize=2.3, color=color)
-                    color_count += 1
+                    # color_count += 1
 
-            color_count = 0
-            style_count += 1
+            color_count += 1
+            style_count = 0
 
+        plt.axhline(y=0.055, color='r', linestyle='--', linewidth=1.5, label='y=0.055')
+
+        plt.legend(['Epsilon Greedy', 'Thompson Sampling', 'Q-Learning', 'UCB', 'KSP-FF'])
+        plt.xlim([-5, 199])
         self._save_plot(file_name=file_name)
         plt.show()
 
@@ -80,12 +84,12 @@ class PlotStats:
         legend_list = list()
         color_count = 0
         style_count = 0
-        for _, sims_dict in self.plot_props['plot_dict'].items():
+        for _, sims_dict in self.props_obj.plot_dict.items():
             for _, info_dict in sims_dict.items():
-                style = self.plot_props['style_list'][style_count]
+                style = self.props_obj.style_list[style_count]
 
                 for y_val, legend_val in zip(y_vals_list, legend_val_list):
-                    color = self.plot_props['color_list'][color_count]
+                    color = self.props_obj.color_list[color_count]
                     plt.plot(info_dict[x_vals], info_dict[y_val], linestyle=style, markersize=2.3, color=color)
 
                     if not force_legend:
@@ -96,7 +100,7 @@ class PlotStats:
                     color_count += 1
 
             color_count = 0
-            style_count += 1
+            style_count = 0
 
         plt.legend(legend_list)
         self._save_plot(file_name=file_name)
@@ -159,14 +163,15 @@ class PlotStats:
         Plots the average blocking probability for each Erlang value.
         """
         self._setup_plot("Average Blocking Prob. vs. Erlang", y_label='Average Blocking Probability',
-                         x_label='Time Steps', y_ticks=True, x_ticks=False, y_lim=[])
+                         x_label='Time Steps', y_ticks=False, x_ticks=False, y_lim=[0, 0.15])
 
         if not ai:
             self._plot_helper_one(x_vals='erlang_list', y_vals_list=['blocking_list'],
-                                  legend_val_list=['Trained US', 'Trained Euro', 'Baseline', 'Combined Model'], force_legend=True, file_name='average_bp')
+                                  legend_val_list=['Trained US', 'Trained Euro', 'Baseline', 'Combined Model'],
+                                  force_legend=True, file_name='average_bp')
         else:
             # TODO: Make block per iter a matrix, for each Erlang
-            self._plot_helper_two(y_vals_list=['block_per_iter'], erlang=700, file_name='bp_e{250}')
+            self._plot_helper_two(y_vals_list=['block_per_iter'], erlang=750, file_name='bp_e750')
 
 
 def main():
@@ -177,6 +182,11 @@ def main():
         'and_filter_list': [
         ],
         'or_filter_list': [
+            ['sim_start', '17_07_02_823817'],
+            ['sim_start', '17_25_05_247014'],
+            ['sim_start', '17_53_02_873894'],
+            ['sim_start', '20_26_35_936142'],
+            ['sim_start', '11_19_35_314638'],
         ],
         'not_filter_list': [
             # ['max_segments', 4],
@@ -184,10 +194,10 @@ def main():
         ]
     }
 
-    sims_info_dict = find_times(dates_dict={'0529': 'USNet'}, filter_dict=filter_dict)
+    sims_info_dict = find_times(dates_dict={'0624': 'NSFNet', '0625': 'NSFNet'}, filter_dict=filter_dict)
     plot_obj = PlotStats(sims_info_dict=sims_info_dict)
 
-    plot_obj.plot_blocking(ai=False)
+    plot_obj.plot_blocking(ai=True)
     # plot_obj.plot_path_length()
     # plot_obj.plot_hops()
     # plot_obj.plot_block_reasons()
