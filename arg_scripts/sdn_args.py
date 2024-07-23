a079d4ef8797a95c99e179bcefb68d5f13cf350b
Ryan McCann <ryan_mccann@student.uml.edu>
2024-07-23 14:22:25 -0400

    Change sdn_args.py to an object

--- arg_scripts/sdn_args.py
+++ arg_scripts/sdn_args.py
@@ -1,19 +1,28 @@
-empty_props = {
-    'path_list': None,
-    'was_routed': None,
-    'topology': None,
-    'net_spec_dict': None,
-    'req_id': None,
-    'source': None,
-    'destination': None,
-    'bandwidth': None,
-    'bandwidth_list': list(),
-    'modulation_list': list(),
-    'core_list': list(),
-    'xt_list': list(),
-    'stat_key_list': ['modulation_list', 'xt_list', 'core_list'],
-    'num_trans': None,
-    'slots_needed': None,
-    'single_core': False,
-    'block_reason': None,
-}
+# pylint: disable=too-few-public-methods
+
+class SDNProps:
+    """
+    Main properties used for the sdn_controller.py script.
+    """
+
+    def __init__(self):
+        self.path_list = None  # List of nodes for the current request
+        self.was_routed = None  # Flag to determine successful route
+        self.topology = None  # Networkx topology
+        self.net_spec_dict = None  # Current network spectrum database
+        self.req_id = None  # Current request ID number
+        self.source = None  # Source node
+        self.destination = None  # Destination node
+        self.bandwidth = None  # Current bandwidth
+        self.bandwidth_list = []  # Multiple bandwidths used (typically for light-segment slicing)
+        self.modulation_list = []  # List of valid modulation formats
+        self.core_list = []  # List of cores used (typically for light-segment slicing)
+        self.xt_list = []  # List of crosstalk calculations for a single request
+        self.stat_key_list = ['modulation_list', 'xt_list', 'core_list']  # Statistical keys used to save results
+        self.num_trans = None  # Number of transponders a single request has used
+        self.slots_needed = None  # Slots needed for the current request
+        self.single_core = False  # Whether to force single-core
+        self.block_reason = None  # Reason for blocking a request
+
+    def __repr__(self):
+        return f"EmptyProps({self.__dict__})"
