30585b174ddec2e75412fae59450dd0f15193ef9
Ryan McCann <ryan_mccann@student.uml.edu>
2024-07-25 17:19:29 -0400

    Clean ml_helpers.py
    Also delete ppo_helpers.py

--- helper_scripts/ml_helpers.py
+++ helper_scripts/ml_helpers.py
@@ -7,22 +7,17 @@ import joblib
 import seaborn as sns
 
 from sklearn.inspection import permutation_importance
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 from helper_scripts.os_helpers import create_dir
 from helper_scripts.sim_helpers import find_path_len, find_core_cong
 
 
-def plot_data(sim_dict, df, erlang):
-    # TODO: Repeat code
-    save_fp = os.path.join('data', 'plots', sim_dict['train_file_path'], 'input_analysis')
-    create_dir(file_path=save_fp)
-
+def _plot_pie(input_df: pd.DataFrame, erlang: float, save_fp: str):
     for column in ['old_bandwidth', 'num_segments', 'longest_reach']:
         plt.figure(figsize=(6, 6), dpi=300)
-        counts = df[column].value_counts()
-        df[column].value_counts().plot(kind='pie', autopct=lambda p: f'{p:.1f}%',
+        counts = input_df[column].value_counts()
+        input_df[column].value_counts().plot(kind='pie', autopct=lambda p: f'{p:.1f}%',
                                        textprops={'color': 'white', 'weight': 'bold'})
         plt.title(f'Pie Chart for {column} - Erlang {erlang}', weight='bold')
 
@@ -32,28 +27,40 @@ def plot_data(sim_dict, df, erlang):
 
         tmp_fp = os.path.join(save_fp, f'pie_chart_{column}_{erlang}.png')
         plt.savefig(tmp_fp, bbox_inches='tight')
-        # plt.close()
-       #  plt.show()
 
+
+def _plot_hist(erlang: float, save_fp: str, input_df: pd.DataFrame):
     for column in ['path_length', 'ave_cong']:
         plt.figure(figsize=(12, 6), dpi=300)
 
         plt.subplot(1, 2, 1)
-        sns.histplot(df[column], kde=True)
+        sns.histplot(input_df[column], kde=True)
         plt.title(f'Histogram for {column} - Erlang {erlang}', weight='bold')
         plt.grid(True)
 
         plt.subplot(1, 2, 2)
-        sns.boxplot(x=df[column])
+        sns.boxplot(x=input_df[column])
         plt.title(f'Box Plot for {column} - Erlang {erlang}', weight='bold')
 
         tmp_fp = os.path.join(save_fp, f'hist_box_{column}_{erlang}.png')
         plt.savefig(tmp_fp, bbox_inches='tight')
-        # plt.show()
-        # plt.close()
 
 
-# TODO: Double check this function as well
+def plot_data(sim_dict: dict, input_df: pd.DataFrame, erlang: float):
+    """
+    Plots data related to machine learning simulation runs.
+
+    :param sim_dict: Relevant information for every simulation.
+    :param input_df: Relevant information for simulations as a dataframe.
+    :param erlang: Traffic volume value.
+    """
+    save_fp = os.path.join('data', 'plots', sim_dict['train_file_path'], 'input_analysis')
+    create_dir(file_path=save_fp)
+
+    _plot_pie(erlang=erlang, input_df=input_df, save_fp=save_fp)
+    _plot_hist(erlang=erlang, input_df=input_df, save_fp=save_fp)
+
+
 def _get_ml_obs(tmp_dict: dict, engine_props: dict, sdn_props: object):
     df_processed = pd.DataFrame(tmp_dict, index=[0])
     df_processed = pd.get_dummies(df_processed, columns=['old_bandwidth'])
@@ -75,9 +82,17 @@ def _get_ml_obs(tmp_dict: dict, engine_props: dict, sdn_props: object):
 
 
 def get_ml_obs(req_dict: dict, engine_props: dict, sdn_props: object):
+    """
+    Creates a single entry or observation structured properly for a machine learning model to make a prediction.
+
+    :param req_dict: Holds request information.
+    :param engine_props: Engine properties.
+    :param sdn_props: SDN controller properties.
+    :return: The correctly formatted observation.
+    :rtype: pd.DataFrame
+    """
     path_length = find_path_len(path_list=sdn_props.path_list, topology=engine_props['topology'])
     cong_arr = np.array([])
-    # TODO: Repeat code
     for core_num in range(engine_props['cores_per_link']):
         curr_cong = find_core_cong(core_index=core_num, net_spec_dict=sdn_props.net_spec_dict,
                                    path_list=sdn_props.path_list)
@@ -124,19 +139,17 @@ def save_model(sim_dict: dict, model, algorithm: str, erlang: str):
     joblib.dump(model, save_fp)
 
 
-def process_data(sim_dict, input_df: pd.DataFrame, erlang):
+def process_data(sim_dict: dict, input_df: pd.DataFrame, erlang: float):
     """
     Process data for machine learning model.
 
+    :param sim_dict: Holds relevant simulation information.
     :param input_df: Input dataframe.
+    :param erlang: Traffic volume.
     :return: Modified processed dataframe.
     :rtype: pd.DataFrame
     """
-    # Apply MinMax scaling to 'path_length' and 'ave_cong' columns
-    # scaler = MinMaxScaler()
-    # input_df[['path_length', 'longest_reach', 'ave_cong']] = scaler.fit_transform(
-        # input_df[['path_length', 'longest_reach', 'ave_cong']])
-    plot_data(df=input_df, erlang=erlang, sim_dict=sim_dict)
+    plot_data(input_df=input_df, erlang=erlang, sim_dict=sim_dict)
     df_processed = pd.get_dummies(input_df, columns=['old_bandwidth'])
 
     for col in df_processed.columns:
@@ -146,18 +159,17 @@ def process_data(sim_dict, input_df: pd.DataFrame, erlang):
     return df_processed
 
 
-def even_process_data(input_df: pd.DataFrame, per_slice: bool, erlang, sim_dict):
+def even_process_data(input_df: pd.DataFrame, per_slice: bool, erlang: float, sim_dict: dict):
     """
-    Process data for machine learning model.
+    Process data for a machine learning model.
 
     :param input_df: Input dataframe.
     :param per_slice: Boolean flag to indicate if equal entries for each num_segments are required.
+    :param sim_dict: Holds relevant simulation information.
+    :param erlang: Current traffic volume.
     :return: Modified processed dataframe.
     :rtype: pd.DataFrame
     """
-    # scaler = MinMaxScaler()
-    # input_df[['path_length', 'longest_reach', 'ave_cong']] = scaler.fit_transform(
-    #     input_df[['path_length', 'longest_reach', 'ave_cong']])
     if per_slice:
         unique_segments = input_df['num_segments'].unique()
         dfs = [input_df[input_df['num_segments'] == segment] for segment in unique_segments]
@@ -179,24 +191,17 @@ def even_process_data(input_df: pd.DataFrame, per_slice: bool, erlang, sim_dict)
 
         df_processed = pd.concat([df1, df2, df4, df8]).sample(frac=1, random_state=42)
 
-    plot_data(df=df_processed, erlang=erlang, sim_dict=sim_dict)
-    # df_processed = df_processed.loc[:, ['num_segments', 'longest_reach', 'path_length']]
-    df_processed = pd.get_dummies(df_processed, columns=['old_bandwidth'])
-
-    for col in df_processed.columns:
-        if df_processed[col].dtype == bool:
-            df_processed[col] = df_processed[col].astype(int)
-
-    return df_processed
+    return process_data(sim_dict=sim_dict, input_df=df_processed, erlang=erlang)
 
 
-def plot_feature_importance(sim_dict, model, feature_names, erlang, x_test, y_test):
+def plot_feature_importance(sim_dict: dict, model, feature_names_list: list, erlang: float, x_test: np.array,
+                            y_test: np.array):
     """
     Plots the feature importance for a model.
 
     :param sim_dict: The simulation dictionary.
     :param model: Trained model.
-    :param feature_names: List of feature names.
+    :param feature_names_list: List of feature names.
     :param erlang: The Erlang value.
     :param x_test: The test data.
     :param y_test: The test labels.
@@ -220,7 +225,7 @@ def plot_feature_importance(sim_dict, model, feature_names, erlang, x_test, y_te
     plt.title("Feature Rankings", weight='bold')
     plt.bar(range(len(importances)), importances[indices],
             color="b", align="center")
-    plt.xticks(range(len(importances)), [feature_names[i] for i in indices], rotation='vertical')
+    plt.xticks(range(len(importances)), [feature_names_list[i] for i in indices], rotation='vertical')
     plt.xlim([-1, len(importances)])
 
     save_fp = os.path.join('data', 'plots', sim_dict['train_file_path'])
@@ -229,41 +234,14 @@ def plot_feature_importance(sim_dict, model, feature_names, erlang, x_test, y_te
     save_fp = os.path.join(save_fp, f'feature_rankings_{erlang}.png')
     plt.savefig(save_fp, bbox_inches='tight')
 
-    # plt.show()
-    # plt.close()
 
-
-def plot_confusion(sim_dict: dict, y_test, y_pred, erlang: str, algorithm: str):
-    """
-    Plots a confusion matrix and prints out the accuracy, precision, recall, and F1 score.
-e
-    :param sim_dict: The simulation dictionary.
-    :param y_test: Testing data.
-    :param y_pred: Predictions.
-    :param erlang: The Erlang value.
-    :param algorithm: The algorithm used.
-    """
-    accuracy = accuracy_score(y_test, y_pred)
-    print(erlang, accuracy, algorithm)
-    precision = precision_score(y_test, y_pred, average='weighted')
-    recall = recall_score(y_test, y_pred, average='weighted')
-    f_score = f1_score(y_test, y_pred, average='weighted')
-
-    labels = np.unique(np.concatenate((y_test, y_pred)))
-    # Plot a confusion matrix
-    conf_mat = confusion_matrix(y_test, y_pred, labels=labels)
-    plt.figure(figsize=(10, 8), dpi=300)  # Increase the quality by increasing dpi
-    sns.heatmap(conf_mat, annot=True, fmt='d', xticklabels=labels, yticklabels=labels)
-    print(algorithm)
-    plt.title(f'Confusion Matrix - {algorithm}', weight='bold')
-    plt.xlabel('Predicted Segments', weight='bold')
-    plt.ylabel('Actual Segments', weight='bold')
-
+def _plot_confusion(y_test: np.array, y_pred: np.array, accuracy: float, precision: float, recall: float,
+                    f_score: float, sim_dict: dict, erlang: str):
     # Calculate accuracy for each unique num_segments value
     unique_segments = np.unique(y_test)
     accuracy_per_segment = []
     for segment in unique_segments:
-        mask = (y_test == segment)
+        mask = (y_test == segment)  # pylint: disable=superfluous-parens
         segment_accuracy = accuracy_score(y_test[mask], y_pred[mask])
         accuracy_per_segment.append(f"NS={segment}: {segment_accuracy:.4f}")
 
@@ -282,8 +260,33 @@ e
     save_fp = os.path.join(save_fp, f'confusion_matrix_{erlang}.png')
     plt.savefig(save_fp, bbox_inches='tight')
 
-   #  plt.show()
-    # plt.close()
+
+def plot_confusion(sim_dict: dict, y_test: np.array, y_pred: np.array, erlang: str, algorithm: str):
+    """
+    Plots a confusion matrix and prints out the accuracy, precision, recall, and F1 score.
+
+    :param sim_dict: The simulation dictionary.
+    :param y_test: Testing data.
+    :param y_pred: Predictions.
+    :param erlang: The Erlang value.
+    :param algorithm: The algorithm used.
+    """
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='weighted')
+    recall = recall_score(y_test, y_pred, average='weighted')
+    f_score = f1_score(y_test, y_pred, average='weighted')
+
+    labels = np.unique(np.concatenate((y_test, y_pred)))
+    # Plot a confusion matrix
+    conf_mat = confusion_matrix(y_test, y_pred, labels=labels)
+    plt.figure(figsize=(10, 8), dpi=300)  # Increase the quality by increasing dpi
+    sns.heatmap(conf_mat, annot=True, fmt='d', xticklabels=labels, yticklabels=labels)
+    plt.title(f'Confusion Matrix - {algorithm}', weight='bold')
+    plt.xlabel('Predicted Segments', weight='bold')
+    plt.ylabel('Actual Segments', weight='bold')
+
+    _plot_confusion(y_test=y_test, y_pred=y_pred, accuracy=accuracy, precision=precision, recall=recall,
+                    f_score=f_score, sim_dict=sim_dict, erlang=erlang)
 
 
 def plot_2d_clusters(df_pca: pd.DataFrame):
@@ -300,7 +303,6 @@ def plot_2d_clusters(df_pca: pd.DataFrame):
     plt.xlabel("Principal Component 1 (PC1)")
     plt.ylabel("Principal Component 2 (PC2)")
     plt.colorbar(scatter, label='num_slices')
-    # plt.show()
 
 
 def plot_3d_clusters(df_pca: pd.DataFrame):
@@ -310,13 +312,12 @@ def plot_3d_clusters(df_pca: pd.DataFrame):
     :param df_pca: A dataframe normalized with PCA.
     """
     fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111, projection='3d')
+    axis = fig.add_subplot(111, projection='3d')
 
     # Plot the test data points, colored by their predicted labels
-    scatter = ax.scatter(df_pca["PC1"], df_pca["PC2"], df_pca["PC3"], c=df_pca["predicted_label"], cmap='Set1')
-    ax.set_title("KNN Predicted Labels (PCA-reduced Data)")
-    ax.set_xlabel("Principal Component 1 (PC1)")
-    ax.set_ylabel("Principal Component 2 (PC2)")
-    ax.set_zlabel("Principal Component 3 (PC3)")
+    scatter = axis.scatter(df_pca["PC1"], df_pca["PC2"], df_pca["PC3"], c=df_pca["predicted_label"], cmap='Set1')
+    axis.set_title("KNN Predicted Labels (PCA-reduced Data)")
+    axis.set_xlabel("Principal Component 1 (PC1)")
+    axis.set_ylabel("Principal Component 2 (PC2)")
+    axis.set_zlabel("Principal Component 3 (PC3)")
     fig.colorbar(scatter, label='num_slices')
-    # plt.show()
