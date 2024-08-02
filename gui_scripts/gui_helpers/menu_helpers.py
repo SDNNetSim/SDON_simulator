47bab07372802de6e82ca0e0c24a5171c8a3a210
Ryan McCann <ryan_mccann@student.uml.edu>
2024-08-02 12:26:47 -0400

    Create menu_helpers.py

---
+++ gui_scripts/gui_helpers/menu_helpers.py
@@ -0,0 +1,12 @@
+class MenuHelpers:
+    def __init__(self):
+        raise NotImplementedError
+
+    def create_file_menu(self):
+        raise NotImplementedError
+
+    def create_edit_menu(self):
+        raise NotImplementedError
+
+    def create_help_menu(self):
+        raise NotImplementedError
