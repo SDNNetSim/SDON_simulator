38139839617fa0486d825335ac4749be1d591085
Ryan McCann <ryan_mccann@student.uml.edu>
2024-08-05 11:44:37 -0400

    Lint circle.py

--- gui_scripts/components/circle.py
+++ gui_scripts/components/circle.py
@@ -1,5 +1,6 @@
+# pylint: disable=no-name-in-module
+
 import random
-import math
 
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtGui import QPainter, QPen
@@ -8,42 +9,45 @@ from PyQt5.QtCore import Qt, QPointF
 
 class Circle:
     """
-    Node representation
+    Node representation circle object.
     """
 
-    def __init__(self, x, y, radius, index):
-        # TODO: Add src node information
-        self.x = x
-        self.y = y
-        self.index = index
+    def __init__(self, x_coord, y_coord, radius, index_coord):
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.index_coord = index_coord
         self.radius = radius
         self.connections = []
 
     def add_connection(self, other_circle):
+        """
+        Adds a connection from one node/circle to another.
+
+        :param other_circle: The other circle.
+        """
         self.connections.append(other_circle)
 
     def get_center(self):
-        # TODO: Fine tune it
-        return QPointF(self.x + self.radius / 2.0, self.y + self.radius / 2.0)
+        """
+        Gets the center of a circle.
+        """
+        # TODO: Fine tune this
+        return QPointF(self.x_coord + self.radius / 2.0, self.y_coord + self.radius / 2.0)
 
 
-# TODO: Change to NodeWidget
-#   - Change location setting
-#   - Have a data structure with coordinates for each network
-#       - {'0': {'x': 100, 'y': 50}}
-#       - Make it responsive
-#       - On Overflow (scroll)
 class CirclesWidget(QWidget):
     """
-    Class to represent nodes
+    Widget used to represent nodes.
     """
 
     def __init__(self):
         super().__init__()
-        # don't replace but pass network_mapping to this widget
         self.circles = []
 
-    def paintEvent(self, event):
+    def paint_event(self):
+        """
+        Paints an event.
+        """
         painter = QPainter(self)
         painter.setRenderHint(QPainter.Antialiasing)
         font = painter.font()
@@ -51,12 +55,11 @@ class CirclesWidget(QWidget):
         painter.setFont(font)
         for circle in self.circles:
             painter.setBrush(Qt.black)
-            painter.drawEllipse(circle.x, circle.y, circle.radius, circle.radius)
+            painter.drawEllipse(circle.x_coord, circle.y_coord, circle.radius, circle.radius)
 
-            # TODO: ChatGPT
             painter.setPen(Qt.white)
             center = circle.get_center()
-            painter.drawText(center, str(self.circles.index(circle) + 1))
+            painter.drawTex_coordt(center, str(self.circles.index_coord(circle) + 1))  # pylint: disable=no-member
 
         # Draw connections between circles
         painter.setPen(QPen(Qt.red, 3))
@@ -64,31 +67,37 @@ class CirclesWidget(QWidget):
             for connected_circle in circle.connections:
                 start_point = circle.get_center() - QPointF(19.09, 0)
                 end_point = connected_circle.get_center() - QPointF(19.1, 0)
-                # Draw line between adjusted points
-                # TODO: May need to change per node
                 painter.drawLine(start_point, end_point)
 
-    def generate_circles(self):
-        # self.circles = []
-        # here, number of circles is len(network_mapping)
-        num_circles = 10
-        radius = min(self.width(), self.height()) // 10  # Determine radius based on window size
-        total_width = 0
-        max_height = 0
-        for index in range(num_circles):
-            while True:
-                x = random.randint(0, self.width() - radius)
-                y = random.randint(0, self.height() - radius)
-                if not self.check_overlap(x, y, radius):
-                    break
-            circle = Circle(x, y, radius, index=index)
-            self.circles.append(circle)
-            total_width = max(total_width, x + radius)
-            max_height = max(max_height, y + radius)
+    def max_coord(self, height, width):
+        """
+        Finds the maximum coordinate.
 
-        # need to rethink this part
+        :param height: Max height.
+        :param width: Max width.
+        """
+        raise NotImplementedError
+
+    def generate_circles(self):
+        """
+        Generate circles or topology_coord.
+        """
+        num_circles = 10
+        radius = min(self.width(), self.height()) // 10
+        total_width = 0
+        max_coord_height = 0
+        for index_coord in range(num_circles):
+            while True:
+                x_coord = random.randint(0, self.width() - radius)
+                y_coord = random.randint(0, self.height() - radius)
+                if not self.check_overlap(x_coord, y_coord, radius):
+                    break
+            circle = Circle(x_coord, y_coord, radius, index_coord=index_coord)
+            self.circles.append(circle)
+            total_width = self.max_coord(total_width, x_coord + radius)
+            max_coord_height = self.max_coord(max_coord_height, y_coord + radius)
+
         for circle in self.circles:
-            # TODO: Make sure connections from topology
             num_connections = random.randint(1, min(3, num_circles - 1))
             connected_circles = random.sample(self.circles, num_connections)
             for connected_circle in connected_circles:
@@ -97,14 +106,15 @@ class CirclesWidget(QWidget):
 
         self.update()
 
-    def check_overlap(self, x, y, radius):
+    def check_overlap(self, x_coord, y_coord, radius):
         """
-        Checks overlap for
+        Checks overlap for nodes.
         """
         for circle in self.circles:
-            dx = circle.x - x
-            dy = circle.y - y
-            distance = (dx ** 2 + dy ** 2) ** 0.5
+            dx_coord = circle.x_coord - x_coord
+            dy_coord = circle.y_coord - y_coord
+            distance = (dx_coord ** 2 + dy_coord ** 2) ** 0.5
             if distance < radius + circle.radius:
                 return True
+
         return False
