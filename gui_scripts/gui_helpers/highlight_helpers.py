92022bd99f98b1d06db5962acdf42d0f85cf728a
Ryan McCann <ryan_mccann@student.uml.edu>
2024-08-05 18:02:56 -0400

    Add highlight_helpers.py

---
+++ gui_scripts/gui_helpers/highlight_helpers.py
@@ -0,0 +1,39 @@
+from PyQt5.QtGui import QSyntaxHighlighter, QTextCharFormat, QColor, QFont
+from PyQt5.QtCore import QRegExp
+
+
+class PythonHighlighter(QSyntaxHighlighter):
+    def __init__(self, document):
+        super().__init__(document)
+
+        # Define the formatting for keywords
+        keyword_format = QTextCharFormat()
+        keyword_format.setForeground(QColor("blue"))
+        keyword_format.setFontWeight(QFont.Bold)
+        keywords = [
+            "\\bclass\\b", "\\bdef\\b", "\\bif\\b", "\\belse\\b", "\\belif\\b",
+            "\\bwhile\\b", "\\bfor\\b", "\\btry\\b", "\\bexcept\\b", "\\bfinally\\b",
+            "\\bwith\\b", "\\bas\\b", "\\bimport\\b", "\\bfrom\\b", "\\breturn\\b",
+            "\\bpass\\b", "\\bbreak\\b", "\\bcontinue\\b", "\\braise\\b"
+        ]
+        self.highlighting_rules = [(QRegExp(pattern), keyword_format) for pattern in keywords]
+
+        # Define the formatting for strings
+        string_format = QTextCharFormat()
+        string_format.setForeground(QColor("green"))
+        self.highlighting_rules.append((QRegExp("\".*\""), string_format))
+        self.highlighting_rules.append((QRegExp("\'.*\'"), string_format))
+
+        # Define the formatting for comments
+        comment_format = QTextCharFormat()
+        comment_format.setForeground(QColor("red"))
+        self.highlighting_rules.append((QRegExp("#[^\n]*"), comment_format))
+
+    def highlightBlock(self, text):
+        for pattern, format in self.highlighting_rules:
+            expression = QRegExp(pattern)
+            index = expression.indexIn(text)
+            while index >= 0:
+                length = expression.matchedLength()
+                self.setFormat(index, length, format)
+                index = expression.indexIn(text, index + length)
