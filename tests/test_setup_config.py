d7eb158d38042b8efa83335aa5c456b73d631b4a
Ryan McCann <ryan_mccann@student.uml.edu>
2024-08-07 15:26:58 -0400

    Revamp test_setup_config.py

--- tests/test_setup_config.py
+++ tests/test_setup_config.py
@@ -1,18 +1,21 @@
 import unittest
 import os
+from unittest.mock import patch
 from config_scripts.setup_config import read_config
 from config_scripts.parse_args import parse_args
 
 
 class TestReadConfig(unittest.TestCase):
     """
-    Tests setup_config.py
+    Tests setup_config.py.
     """
 
     def setUp(self):
         self.valid_conf = os.path.join('tests', 'fixtures', 'valid_config.ini')
         self.invalid_conf = os.path.join('tests', 'fixtures', 'invalid_config.ini')
-        self.args_obj = parse_args()
+        self.mock_args = ['program_name']
+
+        os.makedirs('tests/ini', exist_ok=True)
 
     @classmethod
     def tearDownClass(cls):
@@ -20,38 +23,78 @@ class TestReadConfig(unittest.TestCase):
         Removes previously created directory.
         """
         try:
-            remove_fp = os.path.join('tests', 'ini')
-            os.remove(remove_fp)
+            os.rmdir('tests/ini')
         except FileNotFoundError:
             pass
+        except OSError:
+            for root, dirs, files in os.walk('tests/ini', topdown=False):
+                for name in files:
+                    os.remove(os.path.join(root, name))
+                for name in dirs:
+                    os.rmdir(os.path.join(root, name))
+            os.rmdir('tests/ini')
 
+    @patch('sys.argv', ['program_name'])
     def test_successful_config_read(self):
         """
         Test successful configuration file.
         """
-        config_dict = read_config(self.args_obj, self.valid_conf)
+        args_obj = parse_args()
+        config_dict = read_config(args_obj, self.valid_conf)
         self.assertIsNotNone(config_dict)
+        self.assertIn('s1', config_dict)
+        self.assertIsInstance(config_dict['s1'], dict)
 
+    @patch('sys.argv', ['program_name'])
     def test_missing_config_file(self):
         """
-        Test unsuccessful configuration file.
+        Test handling of a missing configuration file.
         """
+        args_obj = parse_args()
         with self.assertRaises(ValueError):
-            read_config(self.args_obj, 'None')
+            read_config(args_obj, 'None')
 
+    @patch('sys.argv', ['program_name'])
     def test_invalid_config_read(self):
         """
-        Test invalid configuration file.
+        Test handling of an invalid configuration file.
         """
+        args_obj = parse_args()
         with self.assertRaises(ValueError) as context:
-            read_config(self.args_obj, self.invalid_conf)
-        self.assertIn("Missing 'route_method' in the general_settings section", str(context.exception))
+            read_config(args_obj, self.invalid_conf)
+        self.assertIn("Missing 'network' in the topology_settings section", str(context.exception))
 
+    @patch('sys.argv', ['program_name'])
     def test_command_line_input(self):
         """
-        Test command line input.
+        Test overriding configuration values with command line input.
         """
-        self.args_obj['holding_time'] = 1000.0
-        config_dict = read_config(self.args_obj, self.valid_conf)
-
+        with patch('sys.argv', ['program_name', '--holding_time', '1000']):
+            args_obj = parse_args()
+        config_dict = read_config(args_obj, self.valid_conf)
         self.assertEqual(config_dict['s1']['holding_time'], 1000.0)
+
+    @patch('sys.argv', ['program_name'])
+    def test_config_with_default_values(self):
+        """
+        Test that default values are set correctly when options are not specified.
+        """
+        args_obj = parse_args()
+        config_dict = read_config(args_obj, self.valid_conf)
+        self.assertIsNone(config_dict['s1'].get('some_optional_parameter'))
+
+    @patch('sys.argv', ['program_name'])
+    def test_multiple_simulation_threads(self):
+        """
+        Test configuration with multiple simulation threads.
+        """
+        args_obj = parse_args()
+        config_dict = read_config(args_obj, self.valid_conf)
+        self.assertIn('s2', config_dict)
+        self.assertIn('s3', config_dict)
+        self.assertIsInstance(config_dict['s2'], dict)
+        self.assertIsInstance(config_dict['s3'], dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
