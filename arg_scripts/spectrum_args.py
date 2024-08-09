82de67129306e424177a733b8ee23a04bcc0fdc1
Ryan McCann <ryan_mccann@student.uml.edu>
2024-08-09 11:34:42 -0400

    Add multi-band to spectrum_args.py

--- arg_scripts/spectrum_args.py
+++ arg_scripts/spectrum_args.py
@@ -16,6 +16,8 @@ class SpectrumProps:
         self.rev_cores_matrix = None  # The reverse of cores matrix e.g., if looking at 3-->4 check 4-->3
         self.core_num = None  # Core number selected for current request
         self.forced_index = None  # Flag to determine forced spectral start slot (Usually for AI algorithms)
+        self.forced_band = None  # Forces a specific band in multi-band scenarios
+        self.curr_band = None  # The chosen band to allocate
         self.start_slot = None  # Start slot assigned for current request
         self.end_slot = None  # End slot assigned for current request
 
