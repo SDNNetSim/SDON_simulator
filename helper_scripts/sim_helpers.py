fixed core congestion calculation based on all bands on core

helper_scripts/sim_helpers.py
@@ -118,14 +118,13 @@ def find_path_cong(path_list: list, net_spec_dict: dict):
     return average_path_cong
 
 
-def find_core_cong(core_index: int, net_spec_dict: dict, path_list: list, band: str):
+def find_core_cong(core_index: int, net_spec_dict: dict, path_list: list):
     """
     Finds the current percentage of congestion on a core along a path.
 
     :param core_index: Index of the core.
     :param net_spec_dict: Network spectrum database.
     :param path_list: Current path.
-    :param band: The current band of allocation.
     :return: The average congestion percentage on the core.
     :rtype: float
     """
@@ -133,13 +132,16 @@ def find_core_cong(core_index: int, net_spec_dict: dict, path_list: list, band: 
     for src, dest in zip(path_list, path_list[1:]):
         src_dest = (src, dest)
         cores_matrix = net_spec_dict[src_dest]['cores_matrix']
-        cores_per_link = float(len(cores_matrix[band]))
-
-        # Every core will have the same number of spectral slots
-        total_slots = len(cores_matrix[band][0])
+        cores_per_link = 0.0
+        total_slots = 0
         slots_taken = 0
-        core_slots_taken = float(len(np.where(cores_matrix[band][core_index] != 0.0)[0]))
-        slots_taken += core_slots_taken
+        for band in cores_matrix:
+            cores_per_link += float(len(cores_matrix[band]))
+
+            # Every core will have the same number of spectral slots
+            total_slots += len(cores_matrix[band][0])
+            core_slots_taken = float(len(np.where(cores_matrix[band][core_index] != 0.0)[0]))
+            slots_taken += core_slots_taken
 
         links_cong_list.append(slots_taken / (total_slots * cores_per_link))
 