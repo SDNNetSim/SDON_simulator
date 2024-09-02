4817dbac549c578fd01b7682c22c7623a51d7c5b
Kwadwo A Bempah <kwadwoasiedu@bempah.com>
2024-09-02 01:23:48 -0400

    Add descriptions for params and obj types

--- gui_scripts/gui_helpers/general_helpers.py
+++ gui_scripts/gui_helpers/general_helpers.py
@@ -5,7 +5,7 @@ import signal
 import subprocess
 import sys
 
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
 
 from gui_scripts.gui_args.config_args import SETTINGS_CONFIG_DICT
 
@@ -87,7 +87,8 @@ class SettingsDialog(QtWidgets.QDialog):  # pylint: disable=too-few-public-metho
             for setting in category["settings"]:
                 label = setting["label"]
                 widget = self.settings_widgets[label]
-                settings[category_name][self._format_label(label)] = self._get_widget_value(widget)
+                settings[category_name][
+                    self._format_label(label)] = self._get_widget_value(widget)
         return {"s1": settings}
 
     @staticmethod
@@ -120,7 +121,8 @@ class SimulationThread(QtCore.QThread):
     output_hints_signal = QtCore.pyqtSignal(str)
 
     def __init__(self):
-        super(SimulationThread, self).__init__()  # pylint: disable=super-with-arguments
+        super(SimulationThread,
+              self).__init__()  # pylint: disable=super-with-arguments
 
         self.simulation_process = None
         self.paused = False
@@ -145,7 +147,8 @@ class SimulationThread(QtCore.QThread):
         self.simulation_process.wait()
 
         self.finished_signal.emit('Simulation done')
-        self.output_hints_signal.emit('Done...cleaning up simulation from thread')
+        self.output_hints_signal.emit(
+            'Done...cleaning up simulation from thread')
 
     def run(self):
         """
@@ -153,7 +156,8 @@ class SimulationThread(QtCore.QThread):
         """
         command = os.path.join(os.getcwd(), "run_sim.py")
 
-        self.simulation_process = subprocess.Popen(  # pylint: disable=consider-using-with
+        self.simulation_process = subprocess.Popen(
+            # pylint: disable=consider-using-with
             args=[sys.executable, command],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -206,11 +210,15 @@ class SimulationThread(QtCore.QThread):
 
 class DirectoryTreeView(QtWidgets.QTreeView):
     """
-    Sets up a new directory tree view
+    Sets up a new directory tree view.
     """
     item_double_clicked_sig = QtCore.pyqtSignal(QtCore.QModelIndex)
 
-    def __init__(self, file_model, parent=None):
+    def __init__(
+        self,
+        file_model: QtWidgets.QFileSystemModel,
+        parent: QtWidgets.QWidget = None
+    ):
         super().__init__(parent)
         self.setSelectionBehavior(QtWidgets.QTreeView.SelectRows)
         self.setSelectionMode(QtWidgets.QTreeView.SingleSelection)
@@ -222,86 +230,108 @@ class DirectoryTreeView(QtWidgets.QTreeView):
         self.is_directory = False
         self.is_cut_operation = False
 
-    def copy_item(self, source_index, cut=False):
+    def copy_item(
+        self,
+        source_index: QtCore.QModelIndex,
+        is_cut_operation: bool = False
+    ):
         """
-        Implements the copy/cut operations in the directory tree view
-
-        :param source_index:    the QModelIndex of the item to be copied or cut
-        :param cut:             boolean to choose between copying and cutting
+        Implements the copy/cut operations in the directory tree view.
+        :param source_index:        the QModelIndex of the item to be copied
+        or cut.
+        :param is_cut_operation:    boolean to choose between copying
+        and cutting.
         """
         # Store the source path and determine if it's a directory
         self.copied_path = self.model.filePath(source_index)
         self.is_directory = QtCore.QFileInfo(self.copied_path).isDir()
-        self.is_cut_operation = cut
+        self.is_cut_operation = is_cut_operation
 
-    def copy_directory(self, source_dir, destination_dir):
+    def copy_directory(
+        self,
+        source_dir: str,
+        destination_dir: str
+    ):
         """
         Copies a directory recursively
 
-        :param source_dir:
-        :param destination_dir:
+        :param source_dir:          Path to old directory location
+        :param destination_dir:     Path to new directory location
         """
-        source = QtCore.QDir(source_dir)
-        destination = QtCore.QDir(destination_dir)
+        source_obj = QtCore.QDir(source_dir)
+        destination_obj = QtCore.QDir(destination_dir)
 
-        destination_path = destination.filePath(
+        destination_path = destination_obj.filePath(
             QtCore.QFileInfo(source_dir).fileName())
-        if not destination.exists(destination_path):
-            destination.mkpath(destination_path)
+        if not destination_obj.exists(destination_path):
+            destination_obj.mkpath(destination_path)
 
-        for file_name in source.entryList(QtCore.QDir.Files):
-            QtCore.QFile.copy(source.absoluteFilePath(file_name),
-                       QtCore.QDir(destination_path).filePath(file_name))
+        for file_name in source_obj.entryList(QtCore.QDir.Files):
+            QtCore.QFile.copy(source_obj.absoluteFilePath(file_name),
+                              QtCore.QDir(destination_path).filePath(file_name))
 
-        for subdir in source.entryList(QtCore.QDir.Dirs | QtCore.QDir.NoDotAndDotDot):
-            self.copy_directory(source.absoluteFilePath(subdir),
+        for subdir in source_obj.entryList(
+                QtCore.QDir.Dirs | QtCore.QDir.NoDotAndDotDot):
+            self.copy_directory(source_obj.absoluteFilePath(subdir),
                                 QtCore.QDir(destination_path).filePath(subdir))
 
         if self.is_cut_operation:
             self.delete_directory(source_dir)
 
-    def paste_item(self, destination_index):
+    def paste_item(
+        self,
+        destination_index: QtCore.QModelIndex
+    ):
         """
-        Pastes a file/folder at destination_index
+        Pastes a file/folder at destination_index.
 
-        :param destination_index:
+        :param destination_index:    index of location where item is to be
+        pasted.
         """
         if self.copied_path:
             destination_dir = self.model.filePath(destination_index)
             if not QtCore.QFileInfo(destination_dir).isDir():
-                destination_dir = QtCore.QFileInfo(destination_dir).absolutePath()
+                destination_dir = QtCore.QFileInfo(
+                    destination_dir).absolutePath()
 
             if self.is_directory:
                 self.copy_directory(self.copied_path, destination_dir)
             else:
                 file_name = QtCore.QFileInfo(self.copied_path).fileName()
                 if QtCore.QFile.copy(self.copied_path,
-                              QtCore.QDir(destination_dir).filePath(file_name)):
-                    pass # basically do nothing
+                                     QtCore.QDir(destination_dir).filePath(
+                                         file_name)):
+                    pass  # basically do nothing
                 else:
-                    QtWidgets.QMessageBox.critical(self, "Error", "Failed to paste the file")
+                    QtWidgets.QMessageBox.critical(self, "Error",
+                                                   "Failed to paste the file")
 
             if self.is_cut_operation:
                 self._delete()
             self.refresh_view()
 
-    def delete_item(self, target_index):
+    def delete_item(
+        self,
+        target_index: QtCore.QModelIndex
+    ):
         """
-        Delete an item from the tree
+        Delete an item from the tree.
+
+        :param target_index:    Index of item to be deleted.
         """
         path = self.model.filePath(target_index)
         is_directory = QtCore.QFileInfo(path).isDir()
 
         if is_directory:
             reply = QtWidgets.QMessageBox.question(self, "Delete Directory",
-                                         f"Are you sure you want to delete the directory '{path}' and all its contents?",
-                                         QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
-                                         QtWidgets.QMessageBox.No)
+                                                   f"Are you sure you want to delete the directory '{path}' and all its contents?",
+                                                   QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+                                                   QtWidgets.QMessageBox.No)
         else:
             reply = QtWidgets.QMessageBox.question(self, "Delete File",
-                                         f"Are you sure you want to delete the file '{path}'?",
-                                         QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
-                                         QtWidgets.QMessageBox.No)
+                                                   f"Are you sure you want to delete the file '{path}'?",
+                                                   QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+                                                   QtWidgets.QMessageBox.No)
 
         if reply == QtWidgets.QMessageBox.Yes:
             if is_directory:
@@ -320,28 +350,34 @@ class DirectoryTreeView(QtWidgets.QTreeView):
             QtCore.QFile.remove(self.copied_path)
         self.refresh_view()  # Refresh the model to reflect changes
 
-    def handle_context_menu(self, position):
+    def handle_context_menu(
+        self,
+        position: QtCore.QModelIndex
+    ):
         """
         Callback function to handle contextEvent signal. The context menu
-        created by this function is rooted at position
+        created by this function is rooted at position.
 
-        :param position:    index of item that generated the signal
+        :param position:    index of item that generated the signal.
         """
         index = self.indexAt(position)
         if index.isValid():
-            menu = QtWidgets.QMenu(self)
-            menu.addSeparator()
-            copy_action = menu.addAction("Copy")
-            cut_action = menu.addAction("Cut")
-            paste_action = menu.addAction("Paste")
-            delete_action = menu.addAction("Delete")
-            menu.addSeparator()
-            action = menu.exec_(self.viewport().mapToGlobal(position))
+            menu_obj = QtWidgets.QMenu(self)
+
+            menu_obj.addSeparator()
+            copy_action = menu_obj.addAction("Copy")
+            cut_action = menu_obj.addAction("Cut")
+            paste_action = menu_obj.addAction("Paste")
+            menu_obj.addSeparator()
+            delete_action = menu_obj.addAction("Delete")
+            menu_obj.addSeparator()
+
+            action = menu_obj.exec_(self.viewport().mapToGlobal(position))
 
             if action == copy_action:
                 self.copy_item(index)
             elif action == cut_action:
-                self.copy_item(index, cut=True)
+                self.copy_item(index, is_cut_operation=True)
             elif action == paste_action:
                 self.paste_item(index)
             elif action == delete_action:
@@ -354,7 +390,8 @@ class DirectoryTreeView(QtWidgets.QTreeView):
         """
         self.setRootIndex(self.model.index(self.model.rootPath()))
 
-    def mousePressEvent(self, event):  # pylint: disable=invalid-name
+    def mousePressEvent(self, event: QtGui.QMouseEvent):  # pylint:
+        # disable=invalid-name
         """
         Overrides mousePressEvent in QTreeView for single click
 
