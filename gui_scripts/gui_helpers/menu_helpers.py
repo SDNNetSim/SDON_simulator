4f6e302ca1654263d24a2f9ceff7ee858ae90133
Kwadwo Bempah <kwadwoasiedu@bempah.com>
2024-09-06 20:04:21 -0400

    Implement ability to load ini config files

--- gui_scripts/gui_helpers/menu_helpers.py
+++ gui_scripts/gui_helpers/menu_helpers.py
@@ -1,6 +1,6 @@
 # pylint: disable=c-extension-no-member
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 
 class MenuHelpers:
@@ -10,21 +10,34 @@ class MenuHelpers:
 
     def __init__(self):
         self.menu_bar_obj = None  # Updated from run_gui.py script
-
         self.file_menu_obj = None
         self.help_menu_obj = None
         self.edit_menu_obj = None
 
-    def open_file(self):
+    @staticmethod
+    def load_config_file():
         """
-        Opens a json or yaml file.
+        Loads a configuration file. Currently supported config file format is INI.
+        If a config file is successfully read, it is copied to ini/run_ini/config.ini
+        since all simulation scripts point to this location for configuration setup. If
+        the file already exists, it is overwritten with the idea that the new file may
+        contain more up-to-date configuration options. The config file path may be
+        updated in the future to point to the ~/.config/sdon/config.ini
+
+        :param:
+        :return None:
         """
-        # Set the file dialog to filter for .yml and .json files only
-        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(
-            self.menu_bar_obj, "Open Configuration File", "", "Config Files (*.yml *.json)"
+        config_file_dialog = QtWidgets.QFileDialog()
+        file_path, _ = config_file_dialog.getOpenFileName(
+            caption="Load Configuration File",
+            filter="INI Files (*.ini)"
         )
-        if file_name:
-            print(f"Selected file: {file_name}")
+        if file_path:
+            target_dir = QtCore.QDir("ini/run_ini")
+            target_path = target_dir.filePath("config.ini")
+            if not target_dir.exists():
+                target_dir.mkpath(".")
+            QtCore.QFile.copy(file_path, target_path)
 
     # TODO: Add to standards and guidelines, must be called "create", if action must end in "action"
     def create_file_menu(self):
@@ -32,9 +45,9 @@ class MenuHelpers:
         Creates the basis of the file menu along with adding an open action.
         """
         self.file_menu_obj = self.menu_bar_obj.addMenu('&File')
-        open_action = QtWidgets.QAction('&Load Configuration from File', self.menu_bar_obj)
-        open_action.triggered.connect(self.open_file)
-        self.file_menu_obj.addAction(open_action)
+        load_config_action = QtWidgets.QAction('&Load Configuration from File', self.menu_bar_obj)
+        load_config_action.triggered.connect(self.load_config_file)
+        self.file_menu_obj.addAction(load_config_action)
 
     def create_edit_menu(self):
         """
