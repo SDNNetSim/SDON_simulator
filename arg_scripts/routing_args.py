8bf5f05b8580fad31622717bbe279f9cdf04f78d
Rezaee <Arash_Rezaee@uml.edu>
2024-09-24 19:31:51 -0400

    add connection index and path index variable for handeling external snr calculation

--- arg_scripts/routing_args.py
+++ arg_scripts/routing_args.py
@@ -9,6 +9,8 @@ class RoutingProps:
         self.paths_matrix = []  # Matrix of potential paths for a single request
         self.mod_formats_matrix = []  # Modulation formats corresponding to each path in paths_matrix
         self.weights_list = []  # Keeping track of one weight of the path (Length, XT, Etc.)
+        self.connection_index = []
+        self.path_index = []
 
         self.input_power = 1e-3  # Power in Watts
         self.freq_spacing = 12.5e9  # Frequency spacing in Hz
