e03e3125bc82690dd83260ccfda3022bd8547445
Rezaee <Arash_Rezaee@uml.edu>
2024-10-01 10:54:14 -0400

    fix pipeline failure

--- arg_scripts/routing_args.py
+++ arg_scripts/routing_args.py
@@ -9,8 +9,8 @@ class RoutingProps:
         self.paths_matrix = []  # Matrix of potential paths for a single request
         self.mod_formats_matrix = []  # Modulation formats corresponding to each path in paths_matrix
         self.weights_list = []  # Keeping track of one weight of the path (Length, XT, Etc.)
-        self.connection_index = []
-        self.path_index = []
+        self.connection_index = [] # Keeping track of source destination index in precalculated routing
+        self.path_index = [] # Keeping track of path index in precalculated routing
 
         self.input_power = 1e-3  # Power in Watts
         self.freq_spacing = 12.5e9  # Frequency spacing in Hz
